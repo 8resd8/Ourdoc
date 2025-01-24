f580e76b494b3611e7b0c8952810a6c56ffb4d30
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-24 23:28:50 +0900

    feat: 이미지 없음 예외클래스

---
+++ backend/src/main/java/com/ssafy/ourdoc/ocr/exception/OCRNoImageException.java
@@ -0,0 +1,7 @@
+package com.ssafy.ourdoc.ocr.exception;
+
+public class OCRNoImageException extends RuntimeException {
+    public OCRNoImageException(String message) {
+        super(message);
+    }
+}
