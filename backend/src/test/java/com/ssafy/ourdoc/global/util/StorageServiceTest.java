6e36b4774a0ca32596c50d0e11f348020aed0ff6
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-27 23:27:25 +0900

    feat: 이미지 업로드 테스트코드 작성

---
+++ backend/src/test/java/com/ssafy/ourdoc/global/util/StorageServiceTest.java
@@ -0,0 +1,33 @@
+package com.ssafy.ourdoc.global.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+@SpringBootTest
+class StorageServiceTest {
+
+	@Autowired
+	private S3StorageService s3StorageService;
+
+	@Test
+	@DisplayName("업로드 결과 확인")
+	void testUploadFile() throws Exception {
+		MockMultipartFile mockFile = new MockMultipartFile(
+			"file",
+			"test-image.png",
+			"image/png",
+			new byte[] {1, 2, 3, 4}  // 간단한 바이트 배열
+		);
+
+		String uploadedUrl = s3StorageService.uploadFile(mockFile);
+
+		// 업로드된 결과가 null 또는 빈 문자열인지 확인
+		assertNotNull(uploadedUrl);
+		assertFalse(uploadedUrl.isEmpty());
+	}
+}
