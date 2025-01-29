83a4fdbf38eddb6551b43a385dead069a70e435b
윤윤호 <yunho_yun@naver.com>
2025-01-29 17:13:37 +0900

    feat: 학급 엔티티 생성
    - 교사와 학생 엔티티에 학급 연관관계 추가[S12P11A405-96]

--- backend/src/main/java/com/ssafy/ourdoc/user/student/entity/Student.java
+++ backend/src/main/java/com/ssafy/ourdoc/user/student/entity/Student.java
@@ -1,35 +1,48 @@
 package com.ssafy.ourdoc.user.student.entity;
 
+import java.time.LocalDateTime;
+
+import com.ssafy.ourdoc.classroom.entity.ClassRoom;
 import com.ssafy.ourdoc.global.entity.BaseTimeEntity;
 import com.ssafy.ourdoc.global.enums.AuthStatus;
 import com.ssafy.ourdoc.user.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Student extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "student_id", nullable = false)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_id", nullable = false)
+	private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @Column(name = "temp_password", nullable = true)
-    private String tempPassword;
+	@Column(name = "temp_password", nullable = true)
+	private String tempPassword;
 
-    @Column(name = "auth_status", nullable = true)
-    private AuthStatus authStatus;
+	@Column(name = "auth_status", nullable = true)
+	private AuthStatus authStatus;
 
-    @Column(name = "certificate_time", nullable = true)
-    private LocalDateTime certificateTime;
+	@Column(name = "certificate_time", nullable = true)
+	private LocalDateTime certificateTime;
 
+	@ManyToOne
+	@JoinColumn(name = "class_id")
+	private ClassRoom classRoom;
 }
--- backend/src/main/java/com/ssafy/ourdoc/user/teacher/entity/Teacher.java
+++ backend/src/main/java/com/ssafy/ourdoc/user/teacher/entity/Teacher.java
@@ -1,37 +1,51 @@
 package com.ssafy.ourdoc.user.teacher.entity;
 
+import java.time.LocalDateTime;
+
+import com.ssafy.ourdoc.classroom.entity.ClassRoom;
 import com.ssafy.ourdoc.global.entity.BaseTimeEntity;
 import com.ssafy.ourdoc.global.enums.EmploymentStatus;
 import com.ssafy.ourdoc.user.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Teacher extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "teacher_id", nullable = false)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "teacher_id", nullable = false)
+	private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @Column(name = "email", nullable = false)
-    private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-    @Column(name = "phone", nullable = false)
-    private String phone;
+	@Column(name = "phone", nullable = false)
+	private String phone;
 
-    @Column(name = "employment_status", nullable = true)
-    private EmploymentStatus employmentStatus;
+	@Column(name = "employment_status", nullable = true)
+	private EmploymentStatus employmentStatus;
 
-    @Column(name = "certificate_time", nullable = true)
-    private LocalDateTime certificateTime;
+	@Column(name = "certificate_time", nullable = true)
+	private LocalDateTime certificateTime;
+
+	@ManyToOne
+	@JoinColumn(name = "class_id")
+	private ClassRoom classRoom;
 }
