4616c999598b53ac625a781b6a477b7912c104e3
김보민 <mokbee1@naver.com>
2025-01-29 17:45:25 +0900

    fix: builder 추가

--- backend/src/main/java/com/ssafy/ourdoc/user/teacher/entity/Teacher.java
+++ backend/src/main/java/com/ssafy/ourdoc/user/teacher/entity/Teacher.java
@@ -5,6 +5,7 @@ import com.ssafy.ourdoc.global.enums.EmploymentStatus;
 import com.ssafy.ourdoc.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Table(name = "teacher")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Teacher extends BaseTimeEntity {
 
@@ -35,4 +37,17 @@ public class Teacher extends BaseTimeEntity {
 
     @Column(name = "certificate_time", nullable = true)
     private LocalDateTime certificateTime;
+
+    @Builder
+    public Teacher(User user,
+                   String email,
+                   String phone,
+                   EmploymentStatus employmentStatus,
+                   LocalDateTime certificateTime) {
+        this.user = user;
+        this.email = email;
+        this.phone = phone;
+        this.employmentStatus = employmentStatus;
+        this.certificateTime = certificateTime;
+    }
 }
