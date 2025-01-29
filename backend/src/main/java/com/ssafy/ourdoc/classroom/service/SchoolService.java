3ac08d647d595d65ba377efb3110650d2f770c9b
윤윤호 <yunho_yun@naver.com>
2025-01-29 21:23:20 +0900

    feat: 나이스 초등학교 검색 기능 구현
    - 초등학교명, 도로명주소(response) 사용[S12P11A405-95]

---
+++ backend/src/main/java/com/ssafy/ourdoc/classroom/service/SchoolService.java
@@ -0,0 +1,91 @@
+package com.ssafy.ourdoc.classroom.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ourdoc.classroom.dto.SchoolResponse;
+
+@Service
+public class SchoolService {
+	@Value("${school.api-url}")
+	private String apiUrl;
+
+	@Value("${school.api-key}")
+	private String apiKey;
+
+	public List<SchoolResponse> parseSchool(String schoolName) throws
+		IOException {
+		String response = getHttpResponse(schoolName);
+		List<SchoolResponse> schoolList = parseBooksFromResponse(response);
+		return schoolList;
+	}
+
+	private String getHttpResponse(String params) throws IOException {
+		HttpURLConnection conn = getConnection(params);
+		StringBuilder response = new StringBuilder();
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(
+			conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300 ? conn.getInputStream() :
+				conn.getErrorStream()))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				response.append(line);
+			}
+		}
+
+		conn.disconnect();
+		return response.toString();
+	}
+
+	private HttpURLConnection getConnection(String params) throws IOException {
+		String queryString = buildQueryString(params);
+		URL url = new URL(apiUrl + "?" + queryString);
+
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+
+		return conn;
+	}
+
+	private String buildQueryString(String schoolName) throws UnsupportedEncodingException {
+		if (schoolName == null || schoolName.isEmpty()) {
+			schoolName = "";
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("KEY=").append(URLEncoder.encode(apiKey, "UTF-8"))
+			.append("&Type=").append(URLEncoder.encode("json", "UTF-8"))
+			.append("&pIndex=").append(URLEncoder.encode("1", "UTF-8"))
+			.append("&pSize=").append(URLEncoder.encode("1000", "UTF-8"))
+			.append("&SCHUL_KND_SC_NM=").append(URLEncoder.encode("초등학교", "UTF-8"))
+			.append("&SCHUL_NM=").append(URLEncoder.encode(schoolName, "UTF-8"));
+
+		return sb.toString();
+	}
+
+	private List<SchoolResponse> parseBooksFromResponse(String response) {
+		List<SchoolResponse> schoolList = new ArrayList<>();
+		JSONObject respJson = new JSONObject(response);
+		JSONArray schoolArr = respJson.getJSONArray("schoolInfo").getJSONObject(1).getJSONArray("row");
+		for (int i = 0; i < schoolArr.length(); i++) {
+			JSONObject schoolObject = schoolArr.getJSONObject(i);
+			String schoolName = schoolObject.getString("SCHUL_NM");
+			String address = schoolObject.getString("ORG_RDNMA");
+
+			schoolList.add(new SchoolResponse(schoolName, address));
+		}
+		return schoolList;
+	}
+}
