bdb1a054402abed2f5941eb55ecc571fe11e15d2
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-30 20:06:04 +0900

    feat: 학급교사 생성됐는지 확인하는 메서드

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherClassRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherClassRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.ourdoc.domain.user.teacher.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.user.teacher.entity.TeacherClass;
 
 public interface TeacherClassRepository extends JpaRepository<TeacherClass, Long> {
+	Optional<TeacherClass> findByUserIdAndClassRoomId(Long userId, Long classRoomId);
 }
