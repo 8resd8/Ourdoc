d9307be75589c8a4a6be366f9cd110f4644da790
김현우 <dkanfjgwls@naver.com>
2025-01-30 16:29:58 +0900

    feat: 학급생성 로직 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 import com.ssafy.ourdoc.domain.classroom.dto.CreateClassRequest;
 import com.ssafy.ourdoc.domain.classroom.entity.ClassRoom;
 import com.ssafy.ourdoc.domain.classroom.repository.ClassRoomRepository;
+import com.ssafy.ourdoc.domain.user.teacher.entity.Teacher;
+import com.ssafy.ourdoc.domain.user.teacher.entity.TeacherClass;
+import com.ssafy.ourdoc.domain.user.teacher.repository.TeacherClassRepository;
+import com.ssafy.ourdoc.global.common.enums.Active;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class ClassService {
 
 	private final ClassRoomRepository classRoomRepository;
+	private final TeacherClassRepository teacherClassRepository;
+	// private final TeacherRepository teacherRepository;
 
 	public void createClass(Long teacherId, CreateClassRequest request) {
 		// 학급 생성
@@ -26,5 +32,16 @@ public class ClassService {
 			.year(Year.of(request.year()))
 			.build();
 		classRoomRepository.save(classRoom);
+
+		// Teacher teacher = teacherRepository.findById(teacherId)
+		// 	.orElseThrow(() -> new IllegalArgumentException("해당 Teacher ID를 찾을 수 없습니다: " + teacherId));
+
+		// teacher_class 테이블에 추가
+		// TeacherClass teacherClass = TeacherClass.builder()
+		// 	.teacher(teacher)
+		// 	.classRoom(classRoom)
+		// 	.active(Active.활성)  // 기본값 설정
+		// 	.build();
+		// teacherClassRepository.save(teacherClass);
 	}
 }
