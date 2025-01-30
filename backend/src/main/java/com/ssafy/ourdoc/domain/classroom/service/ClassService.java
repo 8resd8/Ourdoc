58f0a3a30f1cfd9a478901b4aeab5c7585aa9ce1
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-30 20:08:38 +0900

    feat: 학급을 생성하는 서비스코드 구현

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
@@ -1,6 +1,5 @@
 package com.ssafy.ourdoc.domain.classroom.service;
 
-import java.sql.Date;
 import java.time.Year;
 
 import org.springframework.stereotype.Service;
@@ -10,15 +9,11 @@ import com.ssafy.ourdoc.domain.classroom.entity.ClassRoom;
 import com.ssafy.ourdoc.domain.classroom.entity.School;
 import com.ssafy.ourdoc.domain.classroom.repository.ClassRoomRepository;
 import com.ssafy.ourdoc.domain.classroom.repository.SchoolRepository;
-import com.ssafy.ourdoc.domain.user.entity.User;
-import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.domain.user.teacher.entity.Teacher;
 import com.ssafy.ourdoc.domain.user.teacher.entity.TeacherClass;
 import com.ssafy.ourdoc.domain.user.teacher.repository.TeacherClassRepository;
 import com.ssafy.ourdoc.domain.user.teacher.repository.TeacherRepository;
 import com.ssafy.ourdoc.global.common.enums.Active;
-import com.ssafy.ourdoc.global.common.enums.Gender;
-import com.ssafy.ourdoc.global.common.enums.UserType;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -32,23 +27,20 @@ public class ClassService {
 	private final TeacherClassRepository teacherClassRepository;
 	private final TeacherRepository teacherRepository;
 	private final SchoolRepository schoolRepository;
-	private final UserRepository userRepository;
 
 	public void createClass(Long teacherId, CreateClassRequest request) {
-		// teacherId로 user가져오기
-		// Teacher findTeacher = teacherRepository.findById(teacherId).get();
-		// User user = userRepository.findById(findTeacher.getUser().getId()).get();
-		//
-		// School findSchool = schoolRepository.findBySchoolName(request.schoolName())
-		// 	.orElseThrow(() -> new IllegalArgumentException("학교가 없습니다."));
-
-		// teacherId로 teacher 찾기
 		Teacher findTeacher = teacherRepository.findById(teacherId)
 			.orElseThrow(() -> new IllegalArgumentException("해당 Teacher ID를 찾을 수 없습니다: " + teacherId));
 
 		// 학교 찾기
 		School findSchool = schoolRepository.findBySchoolName(request.schoolName())
 			.orElseThrow(() -> new IllegalArgumentException("학교가 없습니다."));
+
+		// 등록되어 있는 학급
+		if (classRoomRepository.findByGradeAndClassNumberAndYear(request.grade(), request.classNumber(), Year.now())
+			.isPresent()) {
+			throw new IllegalArgumentException("이미 등록된 학급이 있습니다.");
+		}
 
 		// 학급 생성
 		ClassRoom classRoom = ClassRoom.builder()
@@ -60,12 +52,13 @@ public class ClassService {
 		classRoomRepository.save(classRoom);
 
 
+
 		// 학급교사 테이블에도 생성
-		// TeacherClass teacherClass = TeacherClass.builder()
-		// 	.user(findTeacher.getUser())
-		// 	.classRoom(classRoom)
-		// 	.active(Active.활성)  // 기본값 설정
-		// 	.build();
-		// teacherClassRepository.save(teacherClass);
+		TeacherClass teacherClass = TeacherClass.builder()
+			.user(findTeacher.getUser())
+			.classRoom(classRoom)
+			.active(Active.활성)  // 기본값 설정
+			.build();
+		teacherClassRepository.save(teacherClass);
 	}
 }
