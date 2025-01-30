06c590e81c0854073eed2c3a110c2e7d9205c2e2
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-30 12:25:19 +0900

    refactor: OCRService는 기본변환, 향상서비스를 조립하는 역할로 수정

---
+++ backend/src/main/java/com/ssafy/ourdoc/global/ocr/service/OCRService.java
@@ -0,0 +1,23 @@
+package com.ssafy.ourdoc.global.ocr.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ssafy.ourdoc.global.ocr.dto.HandOCRResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OCRService {
+
+	private final OCRBasicService ocrBasicService;
+	private final OCREnhancerService ocrEnhancerService;
+
+	public HandOCRResponse handOCRConvert(MultipartFile file) {
+		String basicText = ocrBasicService.OCRConvert(file);
+		String enhancedTest = ocrEnhancerService.enhanceText(basicText);
+
+		return new HandOCRResponse(enhancedTest);
+	}
+}
