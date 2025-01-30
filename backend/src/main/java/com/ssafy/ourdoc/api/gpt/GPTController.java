0136e63e22e1bbc1a48bea171ae0bb0118453783
RESD\dkanf <dkanfjgwls@naver.com>
2025-01-30 12:25:46 +0900

    refactor: /api 패키지로 외부 api사용하는 컨트롤러만 분리

---
+++ backend/src/main/java/com/ssafy/ourdoc/api/gpt/GPTController.java
@@ -0,0 +1,33 @@
+package com.ssafy.ourdoc.api.gpt;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ourdoc.global.ai.dto.FeedbackRequest;
+import com.ssafy.ourdoc.global.ai.dto.FeedbackResponse;
+import com.ssafy.ourdoc.global.ai.service.AIFeedbackService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ai")
+public class GPTController {
+
+	private final AIFeedbackService aiFeedbackService;
+
+	@PostMapping("/{studentId}/spelling")
+	public FeedbackResponse checkSpelling(@PathVariable("studentId") Long studentId,
+		@RequestBody FeedbackRequest request) {
+		return aiFeedbackService.spelling(studentId, request);
+	}
+
+	@PostMapping("/{studentId}/feedback")
+	public FeedbackResponse provideFeedback(@PathVariable("studentId") Long studentId,
+		@RequestBody FeedbackRequest request) {
+		return aiFeedbackService.feedback(studentId, request);
+	}
+}
---
+++ backend/src/main/java/com/ssafy/ourdoc/api/ocr/OCRController.java
@@ -0,0 +1,39 @@
+package com.ssafy.ourdoc.api.ocr;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ssafy.ourdoc.global.ocr.dto.HandOCRResponse;
+import com.ssafy.ourdoc.global.ocr.service.OCRService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ocr")
+public class OCRController {
+
+	private final OCRService ocrService;
+
+	@PostMapping("/{studentId}/hand")
+	@ResponseStatus(OK)
+	public HandOCRResponse handOCR(@PathVariable("studentId") Long studentId,
+		@RequestPart(value = "hand_image", required = false) MultipartFile handImageRequest) {
+		HandOCRResponse handOCRResponse = ocrService.handOCRConvert(handImageRequest);
+		return null;
+	}
+
+	@PostMapping
+	@ResponseStatus(OK)
+	public HandOCRResponse handOCRTest(
+		@RequestPart(value = "hand_image", required = false) MultipartFile handImageRequest) {
+		return ocrService.handOCRConvert(handImageRequest);
+	}
+}
