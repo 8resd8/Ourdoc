101ce902f7f5b85b59e89822ab7920fdcdb22150
김현우 <dkanfjgwls@naver.com>
2025-01-30 18:26:12 +0900

    feat: class jpa 저장소

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/repository/ClassRoomRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/repository/ClassRoomRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.ourdoc.domain.classroom.repository;
 
+import java.time.Year;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.classroom.entity.ClassRoom;
 
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
+	Optional<ClassRoom> findByGradeAndClassNumberAndYear(int grade, int classNumber, Year year);
 }
