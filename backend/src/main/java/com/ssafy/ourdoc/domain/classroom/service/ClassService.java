0ca73cf2f8136932e6b8b9df554ae8974bc9de87
김현우 <dkanfjgwls@naver.com>
2025-01-31 09:03:50 +0900

    feat: 응답코드 CREATED 설정

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/service/ClassService.java
@@ -29,20 +29,25 @@ public class ClassService {
 	private final SchoolRepository schoolRepository;
 
 	public void createClass(Long teacherId, CreateClassRequest request) {
-		Teacher findTeacher = teacherRepository.findById(teacherId)
-			.orElseThrow(() -> new IllegalArgumentException("해당 Teacher ID를 찾을 수 없습니다: " + teacherId));
+		Teacher findTeacher = getFindTeacher(teacherId);
+		School findSchool = getFindSchool(request);
 
-		// 학교 찾기
-		School findSchool = schoolRepository.findBySchoolName(request.schoolName())
-			.orElseThrow(() -> new IllegalArgumentException("학교가 없습니다."));
+		isExistClass(request);
 
-		// 등록되어 있는 학급
-		if (classRoomRepository.findByGradeAndClassNumberAndYear(request.grade(), request.classNumber(), Year.now())
-			.isPresent()) {
-			throw new IllegalArgumentException("이미 등록된 학급이 있습니다.");
-		}
+		ClassRoom classRoom = creatClassRoom(request, findSchool);
+		createTeacherClass(findTeacher, classRoom);
+	}
 
-		// 학급 생성
+	private TeacherClass createTeacherClass(Teacher findTeacher, ClassRoom classRoom) {
+		TeacherClass teacherClass = TeacherClass.builder()
+			.user(findTeacher.getUser())
+			.classRoom(classRoom)
+			.active(Active.활성)  // 기본값 설정
+			.build();
+		return teacherClassRepository.save(teacherClass);
+	}
+
+	private ClassRoom creatClassRoom(CreateClassRequest request, School findSchool) {
 		ClassRoom classRoom = ClassRoom.builder()
 			.school(findSchool)
 			.classNumber(request.classNumber())
@@ -50,15 +55,25 @@ public class ClassService {
 			.year(Year.of(request.year()))
 			.build();
 		classRoomRepository.save(classRoom);
+		return classRoom;
+	}
 
+	private void isExistClass(CreateClassRequest request) {
+		if (classRoomRepository.findByGradeAndClassNumberAndYear(request.grade(), request.classNumber(), Year.now())
+			.isPresent()) {
+			throw new IllegalArgumentException("이미 등록된 학급이 있습니다.");
+		}
+	}
 
+	private School getFindSchool(CreateClassRequest request) {
+		School findSchool = schoolRepository.findBySchoolName(request.schoolName())
+			.orElseThrow(() -> new IllegalArgumentException("학교가 없습니다."));
+		return findSchool;
+	}
 
-		// 학급교사 테이블에도 생성
-		TeacherClass teacherClass = TeacherClass.builder()
-			.user(findTeacher.getUser())
-			.classRoom(classRoom)
-			.active(Active.활성)  // 기본값 설정
-			.build();
-		teacherClassRepository.save(teacherClass);
+	private Teacher getFindTeacher(Long teacherId) {
+		Teacher findTeacher = teacherRepository.findById(teacherId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 Teacher ID를 찾을 수 없습니다: " + teacherId));
+		return findTeacher;
 	}
 }
