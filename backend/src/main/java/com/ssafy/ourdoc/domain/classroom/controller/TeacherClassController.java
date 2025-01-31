4e0431c31ddbe081cfc0ee3dc3b5bcc180f177bf
김현우 <dkanfjgwls@naver.com>
2025-01-31 09:04:10 +0900

    refactor: 메서드 분리 진행

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/TeacherClassController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/TeacherClassController.java
@@ -1,9 +1,11 @@
 package com.ssafy.ourdoc.domain.classroom.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.classroom.dto.CreateClassRequest;
@@ -19,9 +21,9 @@ public class TeacherClassController {
 	private final ClassService classService;
 
 	@PostMapping("/{teacherId}/class")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void createClassRoom(@PathVariable("teacherId") Long teacherId,
 		@RequestBody CreateClassRequest request) {
 		classService.createClass(teacherId, request);
-
 	}
 }
