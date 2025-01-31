eec7fee2857892423dd34a270a13d2dc3e0493a3
김보민 <mokbee1@naver.com>
2025-01-31 16:39:31 +0900

    refactor: controller 하나로 통합

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/controller/TeacherController.java
@@ -0,0 +1,40 @@
+package com.ssafy.ourdoc.domain.user.teacher.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ourdoc.domain.user.teacher.dto.TeacherSignupRequest;
+import com.ssafy.ourdoc.domain.user.teacher.service.TeacherService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
+public class TeacherController {
+
+	private final TeacherService teacherService;
+
+	// 1. 교사 회원가입
+	@PostMapping("/signup")
+	public ResponseEntity<String> signup(@RequestBody TeacherSignupRequest request) {
+		log.info("교사 회원가입 진입");
+		Long teacherId = teacherService.signup(request);
+		return ResponseEntity.ok("교사 회원가입 완료. teacher_id = " + teacherId);
+	}
+
+	// 2. QR 생성
+	@GetMapping(value = "/{teacherId}/code", produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<byte[]> generateTeacherInviteCode(@PathVariable Long teacherId) {
+		byte[] qrImage = teacherService.generateTeacherClassQr(teacherId);
+		return ResponseEntity.ok(qrImage);
+	}
+}
