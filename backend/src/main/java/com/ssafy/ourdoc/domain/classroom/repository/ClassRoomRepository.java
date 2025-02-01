6d9d96a133ede67d1588458f709a91f322e1e55a
윤윤호 <yunho_yun@naver.com>
2025-02-01 12:17:10 +0900

    fix: 학급 찾기 관련 누락된 repo 추가
    - 학교, 학년, 반번호로 찾는 메서드 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/repository/ClassRoomRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/repository/ClassRoomRepository.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.classroom.entity.ClassRoom;
+import com.ssafy.ourdoc.domain.classroom.entity.School;
 
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
 	Optional<ClassRoom> findByGradeAndClassNumberAndYear(int grade, int classNumber, Year year);
+
+	Optional<ClassRoom> findBySchoolAndGradeAndClassNumber(School school, int grade, int classNumber);
 }
