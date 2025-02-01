616e668d29c0eb4601f18480bb9e25be0f3f1e96
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-01 20:05:57 +0900

    feat: 알림수신자 엔티티에 읽음표시 메서드 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/entity/NotificationRecipient.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/entity/NotificationRecipient.java
@@ -1,21 +1,27 @@
 package com.ssafy.ourdoc.domain.notification.entity;
 
+import java.time.LocalDateTime;
+
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.common.entity.BaseTimeEntity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
-@Table(
-	name = "notification_recipient"
-)
+@Table(name = "notification_recipient")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationRecipient extends BaseTimeEntity {
 
@@ -39,6 +45,10 @@ public class NotificationRecipient extends BaseTimeEntity {
 	public NotificationRecipient(Notification notification, User recipient, LocalDateTime readTime) {
 		this.notification = notification;
 		this.recipient = recipient;
+		this.readTime = readTime;
+	}
+
+	public void markAsRead(LocalDateTime readTime) {
 		this.readTime = readTime;
 	}
 }
