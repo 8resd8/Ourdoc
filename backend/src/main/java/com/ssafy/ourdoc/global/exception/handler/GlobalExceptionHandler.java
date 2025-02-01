b1e93dbe76f1557ee0caf2f157930af6e7d9a4da
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-01 21:26:01 +0900

    feat: NoSuchElementException 예외처리 추가

--- backend/src/main/java/com/ssafy/ourdoc/global/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/ourdoc/global/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ssafy.ourdoc.global.exception.handler;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,17 +15,22 @@ import jakarta.servlet.http.HttpServletRequest;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
-        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getRequestURI());
-    }
-
-    @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(Exception ex, HttpServletRequest request) {
         return ErrorResponse.toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request.getRequestURI());
     }
 
     @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleForbiddenException(Exception ex, HttpServletRequest request) {
         return ErrorResponse.toResponseEntity(HttpStatus.FORBIDDEN, ex.getMessage(), request.getRequestURI());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex, HttpServletRequest request) {
+        return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage(), request.getRequestURI());
     }
 }
