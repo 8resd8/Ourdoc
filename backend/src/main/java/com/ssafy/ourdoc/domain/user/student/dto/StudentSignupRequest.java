3cf25db45983c455c9015657292d44b2b84df955
윤윤호 <yunho_yun@naver.com>
2025-02-01 12:15:26 +0900

    fix: 학생 회원가입 RequestDto 내 누락 필드 추가[S12P11A405-121]

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/dto/StudentSignupRequest.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/dto/StudentSignupRequest.java
@@ -1,23 +1,25 @@
 package com.ssafy.ourdoc.domain.user.student.dto;
 
-import com.ssafy.ourdoc.global.common.enums.Active;
-import com.ssafy.ourdoc.global.common.enums.AuthStatus;
-import com.ssafy.ourdoc.global.common.enums.Gender;
-import lombok.Getter;
-
 import java.sql.Date;
-import java.time.LocalDateTime;
+
+import com.ssafy.ourdoc.global.common.enums.Active;
+import com.ssafy.ourdoc.global.common.enums.Gender;
+
+import lombok.Getter;
 
 /**
  * 학생 회원가입 요청 DTO
  */
 @Getter
 public class StudentSignupRequest {
-    // === User 엔티티에 필요한 필드 ===
-    private String name;
-    private String loginId;
-    private String password;
-    private Date birth;
-    private Gender gender;
-    private Active active; // 활성/비활성
+	// === User 엔티티에 필요한 필드 ===
+	private String name;
+	private String loginId;
+	private String password;
+	private String schoolName;
+	private int grade;
+	private int classNumber;
+	private Date birth;
+	private Gender gender;
+	private Active active; // 활성/비활성
 }
