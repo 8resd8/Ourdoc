a514ab33b7d06195df33bfb8e42fde7a15eff853
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-01 23:37:19 +0900

    refactor: 매핑 url 복수형 사용

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/controller/NotificationController.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 public class NotificationController {
 
 	private final NotificationService notificationService;
