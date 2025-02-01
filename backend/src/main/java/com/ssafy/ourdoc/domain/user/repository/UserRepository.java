b8af7b44aa89101f4c0644839d66694de897a08d
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-01 22:16:51 +0900

    feat: querydsl 레포지토리 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.ssafy.ourdoc.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserQueryRepository {
     Optional<User> findByLoginIdAndPassword(String loginId, String password);
     Optional<User> findByLoginId(String loginId);
 
