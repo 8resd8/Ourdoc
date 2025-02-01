61c877c435235c77d31863e482c5cde03c1a1217
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-01 22:35:46 +0900

    feat: 응답 코드추가, 테스트메서드 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/controller/NotificationController.java
@@ -1,14 +1,19 @@
 package com.ssafy.ourdoc.domain.notification.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.ssafy.ourdoc.domain.notification.service.NotificationHistoryService;
 import com.ssafy.ourdoc.domain.notification.service.NotificationService;
+import com.ssafy.ourdoc.global.common.enums.NotificationType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,14 +25,21 @@ public class NotificationController {
 	private final NotificationService notificationService;
 	private final NotificationHistoryService notificationHistoryService;
 
-	@GetMapping("/subscribe")
+	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter subscribe() {
 		return notificationService.subscribe();
 	}
 
 	@PatchMapping("/{recipientId}/read")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void markAsRead(@PathVariable("recipientId") Long recipientId) {
 		notificationHistoryService.markNotificationRead(recipientId);
+	}
+
+	@PostMapping("/send")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void sendNotificationTest() {
+		notificationService.sendNotification(NotificationType.독서록, "독서록 저장했어.");
 	}
 
 }
