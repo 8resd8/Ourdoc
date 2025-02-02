7916cdabfea01c977de6df590216be31157a637f
방승윤 <sybang98@gmail.com>
2025-02-02 16:57:08 +0900

    feat: 독서토론접속자 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/entity/RoomOnline.java
@@ -0,0 +1,44 @@
+package com.ssafy.ourdoc.domain.debate.entity;
+
+import com.ssafy.ourdoc.domain.user.entity.User;
+import com.ssafy.ourdoc.global.common.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "room_online")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoomOnline extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "room_online_id", unique = true, nullable = false)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id", nullable = false)
+	private Room room;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Builder
+	public RoomOnline(Room room, User user) {
+		this.room = room;
+		this.user = user;
+	}
+}
