adf50c32c1707d95b293203b59ae49622f57aea6
윤윤호 <yunho_yun@naver.com>
2025-02-02 15:16:48 +0900

    fix: 도서 검색 request 내 null 처리[S12P11A405-123]

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookRepository.java
@@ -3,9 +3,13 @@ package com.ssafy.ourdoc.domain.book.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ssafy.ourdoc.domain.book.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+	@Query("SELECT b FROM Book b WHERE (:title is null or b.title = :title)"
+		+ "and (:author is null or b.author = :author)"
+		+ "and (:publisher is null or b.publisher = :publisher)")
 	List<Book> findByTitleAndAuthorAndPublisher(String title, String author, String publisher);
 }
--- backend/src/main/java/com/ssafy/ourdoc/domain/book/service/BookService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/service/BookService.java
@@ -10,12 +10,14 @@ import com.ssafy.ourdoc.domain.book.repository.BookRepository;
 import com.ssafy.ourdoc.global.integration.nationallibrary.dto.NationalLibraryBookResponse;
 import com.ssafy.ourdoc.global.integration.nationallibrary.service.NationalLibraryBookService;
 
+import groovy.util.logging.Slf4j;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class BookService {
 	private final BookRepository bookRepository;
 	private final NationalLibraryBookService nationalLibraryBookService;
@@ -25,15 +27,16 @@ public class BookService {
 	}
 
 	public List<Book> searchBook(BookRequest request) {
-		List<Book> books = bookRepository.findByTitleAndAuthorAndPublisher(request.title(), request.author(),
-			request.publisher());
+		String title = request.title().isEmpty() ? null : request.title();
+		String author = request.author().isEmpty() ? null : request.author();
+		String publisher = request.publisher().isEmpty() ? null : request.publisher();
+
+		List<Book> books = bookRepository.findByTitleAndAuthorAndPublisher(title, author, publisher);
 		if (books.isEmpty()) {
 			List<NationalLibraryBookResponse> externalBooks = nationalLibraryBookService.parseBook(request);
 			externalBooks.forEach(response -> registerBook(NationalLibraryBookResponse.toBookEntity(response)));
 
-			books = bookRepository.findByTitleAndAuthorAndPublisher(
-				request.title(), request.author(), request.publisher()
-			);
+			books = bookRepository.findByTitleAndAuthorAndPublisher(title, author, publisher);
 		}
 		return books;
 	}
