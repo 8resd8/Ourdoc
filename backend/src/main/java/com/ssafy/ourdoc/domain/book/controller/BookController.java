ba45b3952f44603e05fabf2d7957624295e9e5e5
윤윤호 <yunho_yun@naver.com>
2025-02-02 16:48:54 +0900

    feat: 도서 관련 컨트롤러 생성[S12P11A405-123]

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookController.java
@@ -1,7 +1,18 @@
 package com.ssafy.ourdoc.domain.book.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ourdoc.domain.book.dto.BookDetailResponse;
+import com.ssafy.ourdoc.domain.book.dto.BookRequest;
+import com.ssafy.ourdoc.domain.book.dto.BookResponse;
+import com.ssafy.ourdoc.domain.book.service.BookService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +21,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/book")
 public class BookController {
 
+	private final BookService bookService;
+
+	@GetMapping
+	public ResponseEntity<List<BookResponse>> getBooks(@RequestBody BookRequest request) {
+		List<BookResponse> books = bookService.searchBook(request);
+		return ResponseEntity.ok(books);
+	}
+
+	@GetMapping("/{bookId}")
+	public ResponseEntity<BookDetailResponse> getBook(@PathVariable Long bookId) {
+		BookDetailResponse book = bookService.getBookDetail(bookId);
+		return ResponseEntity.ok(book);
+	}
 }
