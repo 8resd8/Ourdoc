3f782821399f0c8dd44f3dc64a363fe83025c0eb
김보민 <mokbee1@naver.com>
2025-02-03 17:10:17 +0900

    feat: OncePerRequestFilter를 JwtAuthenticationFilter로 변경

--- backend/src/main/java/com/ssafy/ourdoc/global/config/FilterConfig.java
+++ backend/src/main/java/com/ssafy/ourdoc/global/config/FilterConfig.java
@@ -8,20 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ssafy.ourdoc.global.filter.JwtAuthenticationFilter;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig implements WebMvcConfigurer {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	public FilterConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-	}
-
 	@Bean
-	public FilterRegistrationBean<OncePerRequestFilter> jwtFilter() {
-		FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
+	public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilter() {
+		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
 		registrationBean.setFilter(jwtAuthenticationFilter);
-		registrationBean.addUrlPatterns("/api/*"); // 보호할 URL 패턴 설정
+		registrationBean.addUrlPatterns("/*"); // 보호할 URL 패턴 설정
+		registrationBean.setOrder(1);
 		return registrationBean;
 	}
 }
