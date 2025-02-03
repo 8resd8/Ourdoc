ceb8b2904fb47a4c099a3c28af9589b2cf794024
김현우 <dkanfjgwls@naver.com>
2025-02-03 17:43:02 +0900

    refactor: 안쓰는 메서드 제거

--- backend/src/test/java/com/ssafy/ourdoc/data/entity/UserSample.java
+++ backend/src/test/java/com/ssafy/ourdoc/data/entity/UserSample.java
@@ -7,18 +7,10 @@ import com.ssafy.ourdoc.global.common.enums.Active;
 import com.ssafy.ourdoc.global.common.enums.Gender;
 import com.ssafy.ourdoc.global.common.enums.UserType;
 
-import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.PriorityConstructorArbitraryIntrospector;
-
 public class UserSample {
 
 	private UserSample() {
 	}
-
-	private static FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
-		.defaultNotNull(true)
-		.objectIntrospector(PriorityConstructorArbitraryIntrospector.INSTANCE)
-		.build();
 
 	public static User user(UserType type) {
 		return User.builder()
