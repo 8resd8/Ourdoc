18a4ec3ea39c4e212ec4d4fff5a1a77fe258ad29
김현우 <dkanfjgwls@naver.com>
2025-02-03 17:14:08 +0900

    feat: 검색조건에 따라 검색이 되도록 쿼리구성

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/repository/NotificationQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/repository/NotificationQueryRepositoryImpl.java
@@ -3,15 +3,18 @@ package com.ssafy.ourdoc.domain.notification.repository;
 import static com.ssafy.ourdoc.domain.notification.entity.QNotification.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ourdoc.domain.notification.dto.NotificationConditionRequest;
 import com.ssafy.ourdoc.domain.notification.dto.NotificationDto;
 import com.ssafy.ourdoc.domain.notification.entity.QNotificationRecipient;
+import com.ssafy.ourdoc.global.common.enums.NotificationStatus;
+import com.ssafy.ourdoc.global.common.enums.NotificationType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,35 +24,43 @@ public class NotificationQueryRepositoryImpl implements NotificationQueryReposit
 
 	private final JPAQueryFactory queryFactory;
 
-	// user가 읽지 않은 알림 전체조회
 	@Override
-	public List<NotificationDto> findAllByUserId(Long userId, Pageable pageable) {
-		QNotificationRecipient recipientId = QNotificationRecipient.notificationRecipient;
+	public List<NotificationDto> findAllConditionByUserId(Long userId, NotificationConditionRequest condition,
+		Pageable pageable) {
+		QNotificationRecipient recipient = QNotificationRecipient.notificationRecipient;
 
 		return queryFactory
-			.select(notification)
+			.select(Projections.constructor(NotificationDto.class,
+				notification.id,
+				notification.notificationType,
+				notification.content,
+				notification.createdAt))
 			.from(notification)
-			.join(recipientId).on(notification.id.eq(recipientId.notification.id))
-			.where(userEq(userId, recipientId), notRead(recipientId))
+			.join(recipient).on(notification.id.eq(recipient.notification.id))
+			.where(
+				userEq(userId, recipient),
+				readFilter(recipient, condition.status()),
+				typeFilter(condition.type()))
 			.orderBy(notification.createdAt.desc())
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
-			.fetch()
-			.stream()
-			.map(n -> new NotificationDto(
-				n.getId(),
-				n.getNotificationType(),
-				n.getContent(),
-				n.getCreatedAt()
-			))
-			.collect(Collectors.toList());
+			.fetch();
+	}
+
+	private BooleanExpression typeFilter(NotificationType type) {
+		return type == null ? null : notification.notificationType.eq(type);
 	}
 
 	private BooleanExpression userEq(Long userId, QNotificationRecipient recipient) {
 		return recipient.recipient.id.eq(userId);
 	}
 
-	private BooleanExpression notRead(QNotificationRecipient recipient) {
-		return recipient.readTime.isNull();
+	private BooleanExpression readFilter(QNotificationRecipient recipient, NotificationStatus status) {
+		if (status == NotificationStatus.읽음) {
+			return recipient.readTime.isNotNull();
+		} else if (status == NotificationStatus.안읽음) {
+			return recipient.readTime.isNull();
+		}
+		return null;
 	}
 }
