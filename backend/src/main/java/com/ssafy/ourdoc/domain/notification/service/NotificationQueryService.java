8b7e6fe292ccc291b5fb36a4dc5f0db2a9581dc6
김현우 <dkanfjgwls@naver.com>
2025-02-03 17:16:31 +0900

    feat: 검색조건 요청추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationQueryService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationQueryService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.ourdoc.domain.notification.dto.NotificationConditionRequest;
 import com.ssafy.ourdoc.domain.notification.dto.NotificationDto;
 import com.ssafy.ourdoc.domain.notification.dto.NotificationResponse;
 import com.ssafy.ourdoc.domain.notification.repository.NotificationQueryRepository;
@@ -19,8 +20,10 @@ public class NotificationQueryService {
 
 	private final NotificationQueryRepository notificationQueryRepository;
 
-	public NotificationResponse getUnreadNotifications(Long userId, Pageable pageable) {
-		List<NotificationDto> notificationDtoList = notificationQueryRepository.findAllByUserId(userId, pageable);
+	public NotificationResponse getUnreadNotifications(Long userId, NotificationConditionRequest request,
+		Pageable pageable) {
+		List<NotificationDto> notificationDtoList = notificationQueryRepository.
+			findAllConditionByUserId(userId, request, pageable);
 
 		return new NotificationResponse(notificationDtoList);
 	}
