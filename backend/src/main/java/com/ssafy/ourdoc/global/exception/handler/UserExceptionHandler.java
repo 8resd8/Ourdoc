22c0b0f52f5beac66ae73a1665d6b115564b4ece
김보민 <mokbee1@naver.com>
2025-02-03 09:26:52 +0900

    rename: User 핸들러로 이름 변경

--- backend/src/main/java/com/ssafy/ourdoc/global/exception/handler/UserExceptionHandler.java
+++ backend/src/main/java/com/ssafy/ourdoc/global/exception/handler/UserExceptionHandler.java
@@ -5,16 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.ssafy.ourdoc.global.exception.LoginFailedException;
+import com.ssafy.ourdoc.global.exception.UserFailedException;
 
 /**
  * 로그인 관련 예외를 처리하는 핸들러
  */
 @RestControllerAdvice
-public class LoginExceptionHandler {
+public class UserExceptionHandler {
 
 	@ExceptionHandler
-	public ResponseEntity<String> handleLoginFailedException(LoginFailedException e) {
+	public ResponseEntity<String> handleUserFailedException(UserFailedException e) {
 		return ResponseEntity
 			.status(HttpStatus.UNAUTHORIZED) // HTTP 401 Unauthorized
 			.body(e.getMessage()); // 예외 메시지를 응답에 포함
