dbc99b6fcae18e6c85d4cb4e85438c5ddfd09120
김현우 <dkanfjgwls@naver.com>
2025-02-04 10:31:28 +0900

    feat: Page객체 반환하도록 반환값 수정

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationQueryService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationQueryService.java
@@ -1,7 +1,6 @@
 package com.ssafy.ourdoc.domain.notification.service;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +22,7 @@ public class NotificationQueryService {
 
 	public NotificationListResponse getUnreadNotifications(Long userId, NotificationConditionRequest request,
 		Pageable pageable) {
-		List<NotificationDto> notificationDtoList = notificationQueryRepository.
+		Page<NotificationDto> notificationDtoList = notificationQueryRepository.
 			findAllConditionByUserId(userId, request, pageable);
 
 		return new NotificationListResponse(notificationDtoList);
