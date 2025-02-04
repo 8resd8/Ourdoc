322d35b8e04f011df87f2daab594f57c53f38bfd
김현우 <dkanfjgwls@naver.com>
2025-02-04 14:48:30 +0900

    feat: 유저데이터 샘플 추가

--- backend/src/test/java/com/ssafy/ourdoc/data/entity/UserSample.java
+++ backend/src/test/java/com/ssafy/ourdoc/data/entity/UserSample.java
@@ -7,46 +7,64 @@ import com.ssafy.ourdoc.global.common.enums.Active;
 import com.ssafy.ourdoc.global.common.enums.Gender;
 import com.ssafy.ourdoc.global.common.enums.UserType;
 
-import groovy.transform.builder.Builder;
-
 public class UserSample {
 
 	private UserSample() {
 	}
 
+	private static String defaultName = "테스트이름";
+	private static String defaultLoginId = "test";
+	private static String defaultPassword = "test1234";
+	private static Active defaultActive = Active.활성;
+	private static Gender defaultGender = Gender.남;
+	private static UserType defaultUserType = UserType.학생;
+	private static Date defaultBirthday = new Date(2000, 1, 18);
+
 	public static User user(UserType type) {
 		return User.builder()
-			.active(Active.활성)
-			.birth(new Date(System.currentTimeMillis()))
-			.loginId("test")
-			.password("testpwd")
+			.active(defaultActive)
+			.birth(defaultBirthday)
+			.loginId(defaultLoginId)
+			.password(defaultPassword)
 			.userType(type)
-			.name("테스트이름")
-			.gender(Gender.남)
+			.name(defaultName)
+			.gender(defaultGender)
 			.build();
 	}
 
 	public static User user(UserType type, String name) {
 		return User.builder()
-			.active(Active.활성)
-			.birth(new Date(System.currentTimeMillis()))
-			.loginId("test")
-			.password("testpwd")
+			.active(defaultActive)
+			.birth(defaultBirthday)
+			.loginId(defaultLoginId)
+			.password(defaultLoginId)
 			.userType(type)
 			.name(name)
-			.gender(Gender.남)
+			.gender(defaultGender)
 			.build();
 	}
 
 	public static User user(UserType type, String loginId, String password) {
 		return User.builder()
-			.active(Active.활성)
-			.birth(new Date(System.currentTimeMillis()))
+			.active(defaultActive)
+			.birth(defaultBirthday)
 			.loginId(loginId)
 			.password(password)
 			.userType(type)
-			.name("테스트이름")
-			.gender(Gender.남)
+			.name(defaultName)
+			.gender(defaultGender)
+			.build();
+	}
+
+	public static User user() {
+		return User.builder()
+			.active(defaultActive)
+			.birth(defaultBirthday)
+			.loginId(defaultLoginId)
+			.password(defaultPassword)
+			.userType(defaultUserType)
+			.name(defaultName)
+			.gender(defaultGender)
 			.build();
 	}
 
