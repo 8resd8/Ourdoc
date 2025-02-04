fc11bc7968affb140f61f3b2cdde02f23c7a1faf
윤윤호 <yunho_yun@naver.com>
2025-02-04 23:25:23 +0900

    feat: 관심 도서 등록, 삭제, 목록 조회 컨트롤러 구현

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookController.java
@@ -3,16 +3,22 @@ package com.ssafy.ourdoc.domain.book.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.book.dto.BookDetailResponse;
+import com.ssafy.ourdoc.domain.book.dto.BookFavoriteRequest;
 import com.ssafy.ourdoc.domain.book.dto.BookRequest;
 import com.ssafy.ourdoc.domain.book.dto.BookResponse;
+import com.ssafy.ourdoc.domain.book.service.BookFavoriteService;
 import com.ssafy.ourdoc.domain.book.service.BookService;
+import com.ssafy.ourdoc.domain.user.entity.User;
+import com.ssafy.ourdoc.global.annotation.Login;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
 
 	private final BookService bookService;
+	private final BookFavoriteService bookFavoriteService;
 
 	@GetMapping
 	public ResponseEntity<List<BookResponse>> getBooks(@RequestBody BookRequest request) {
@@ -33,5 +40,21 @@ public class BookController {
 	public ResponseEntity<BookDetailResponse> getBook(@PathVariable("bookId") Long bookId) {
 		BookDetailResponse book = bookService.getBookDetail(bookId);
 		return ResponseEntity.ok(book);
+	}
+
+	@PostMapping("/favorite")
+	public void addFavorite(@RequestBody BookFavoriteRequest request, @Login User user) {
+		bookFavoriteService.addBookFavorite(request, user);
+	}
+
+	@GetMapping("/favorite")
+	public ResponseEntity<List<BookResponse>> getFavorite(@Login User user) {
+		List<BookResponse> books = bookFavoriteService.getBookFavorites(user);
+		return ResponseEntity.ok(books);
+	}
+
+	@DeleteMapping("/favorite")
+	public void deleteFavorite(@RequestBody BookFavoriteRequest request, @Login User user) {
+		bookFavoriteService.deleteBookFavorite(request, user);
 	}
 }
