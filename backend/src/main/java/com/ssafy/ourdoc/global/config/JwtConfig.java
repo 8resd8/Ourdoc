cee95b1213d4beacaf39b75f701bf37a6fd2f3dc
김보민 <mokbee1@naver.com>
2025-02-04 16:50:29 +0900

    feat: refreshToken 만료시간 추가

--- backend/src/main/java/com/ssafy/ourdoc/global/config/JwtConfig.java
+++ backend/src/main/java/com/ssafy/ourdoc/global/config/JwtConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.ourdoc.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,14 +10,22 @@ public class JwtConfig {
 	@Value("${jwt.secret-key}")
 	private String secretKey;
 
-	@Value("${jwt.expiration}")
-	private long expiration;
+	@Value("${jwt.access-expiration}")
+	private long accessExpiration;
 
+	@Value("${jwt.refresh-expiration}")
+	private long refreshExpiration;
+
+	@Bean
 	public String getSecretKey() {
 		return secretKey;
 	}
 
-	public long getExpiration() {
-		return expiration;
+	@Bean
+	public long getAccessExpiration() {
+		return accessExpiration;
 	}
+
+	@Bean
+	public long getRefreshExpiration() {return refreshExpiration; }
 }
