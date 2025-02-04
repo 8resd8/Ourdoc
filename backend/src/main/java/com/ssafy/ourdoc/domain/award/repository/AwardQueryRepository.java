71500f07676e1bd539ea5b5b4dc02cbc913495a7
김현우 <dkanfjgwls@naver.com>
2025-02-04 17:04:39 +0900

    feat: userId를 가지고 조회하도록 쿼리 구성

--- backend/src/main/java/com/ssafy/ourdoc/domain/award/repository/AwardQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/award/repository/AwardQueryRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.ourdoc.domain.award.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.ssafy.ourdoc.domain.award.dto.AwardDto;
 
 public interface AwardQueryRepository {
 
-	List<AwardDto> findAllAward();
+	List<AwardDto> findAllAwardByUserId(Long userId);
+
+	Optional<AwardDto> findAwardByUserId(Long userId, Long awardId);
 }
