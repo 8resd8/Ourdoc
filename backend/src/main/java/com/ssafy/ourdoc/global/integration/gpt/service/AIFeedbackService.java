11d342c1953ae316b485243843a849cbe6093cb5
김현우 <dkanfjgwls@naver.com>
2025-02-04 15:24:43 +0900

    feat: 실제 로그인한 유저정보로 받도록 구현

--- backend/src/main/java/com/ssafy/ourdoc/global/integration/gpt/service/AIFeedbackService.java
+++ backend/src/main/java/com/ssafy/ourdoc/global/integration/gpt/service/AIFeedbackService.java
@@ -3,6 +3,10 @@ package com.ssafy.ourdoc.global.integration.gpt.service;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.ourdoc.domain.user.entity.User;
+import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
+import com.ssafy.ourdoc.domain.user.student.repository.StudentClassRepository;
+import com.ssafy.ourdoc.domain.user.student.repository.StudentRepository;
 import com.ssafy.ourdoc.global.integration.gpt.dto.FeedbackRequest;
 import com.ssafy.ourdoc.global.integration.gpt.dto.FeedbackResponse;
 import com.ssafy.ourdoc.global.util.Prompt;
@@ -14,20 +18,28 @@ import lombok.RequiredArgsConstructor;
 public class AIFeedbackService {
 
 	private final ChatModel chatModel;
+	private final StudentClassRepository studentClassRepository;
 
-	public FeedbackResponse spelling(Long studentId, FeedbackRequest request) {
-		// studentId로 class와 조인해 학년 찾기
-		int studentGrade = 1;
+	public FeedbackResponse spelling(User user, FeedbackRequest request) {
+		int studentGrade = getStudentGrade(user);
 		String aiSpellingFeedback = chatModel.call(Prompt.feedback(studentGrade, request.content()));
 
 		return new FeedbackResponse(aiSpellingFeedback);
 	}
 
-	public FeedbackResponse feedback(Long studentId, FeedbackRequest request) {
-		// studentId로 class와 조인해 학년 찾기
-		int studentGrade = 1;
+	public FeedbackResponse feedback(User user, FeedbackRequest request) {
+		int studentGrade = getStudentGrade(user);
 		String aiSpellingFeedback = chatModel.call(Prompt.feedback(studentGrade, request.content()));
 
 		return new FeedbackResponse(aiSpellingFeedback);
+	}
+
+	private int getStudentGrade(User user) {
+		StudentClass studentClass = getStudentClass(user);
+		return studentClass.getClassRoom().getGrade();
+	}
+
+	private StudentClass getStudentClass(User user) {
+		return studentClassRepository.findStudentClassByUserId(user.getId()).orElseThrow();
 	}
 }
