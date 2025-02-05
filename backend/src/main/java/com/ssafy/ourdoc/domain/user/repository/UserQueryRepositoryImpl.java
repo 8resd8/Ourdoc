d11550ea0eebfdd3713db0f3172b62b25b30f08b
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-05 21:43:03 +0900

    refactor: 학급학생에서 학급id 조회하도록 변경

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
@@ -1,7 +1,7 @@
 package com.ssafy.ourdoc.domain.user.repository;
 
 import static com.ssafy.ourdoc.domain.user.entity.QUser.*;
-import static com.ssafy.ourdoc.domain.user.student.entity.QStudent.*;
+import static com.ssafy.ourdoc.domain.user.student.entity.QStudentClass.*;
 import static com.ssafy.ourdoc.domain.user.teacher.entity.QTeacherClass.*;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -14,23 +14,13 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 
 	private final JPAQueryFactory queryFactory;
 
-	@Override
-	public Long findTeacherIdByStudentId(Long studentId) {
-		return queryFactory
-			.select(teacherClass.id)
-			.from(student) // StudentClass로 변경해야함.
-			.join(teacherClass).on(student.classRoom.id.eq(teacherClass.classRoom.id))
-			.where(student.user.id.eq(studentId))
-			.fetchOne();
-	}
-
 	// 학생의 담당교사 찾기
 	@Override
 	public User findTeachersByStudentClassId(Long studentUserId) {
 		// 학생의 classId 가져오기
-		Long classId = queryFactory.select(student.classRoom.id)
-			.from(student)
-			.where(student.user.id.eq(studentUserId))
+		Long classId = queryFactory.select(studentClass.classRoom.id)
+			.from(studentClass)
+			.where(studentClass.user.id.eq(studentUserId))
 			.fetchOne();
 
 		// classId로 해당 클래스의 교사 조회
