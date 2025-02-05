3f1c12663671c2cac4a588bf8e4b8c74b2c0a02a
김현우 <dkanfjgwls@naver.com>
2025-02-05 20:15:52 +0900

    feat: 로그인 유저 반영

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationHistoryService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/service/NotificationHistoryService.java
@@ -15,6 +15,7 @@ import com.ssafy.ourdoc.domain.notification.repository.NotificationRepository;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.global.common.enums.NotificationType;
+import com.ssafy.ourdoc.global.common.enums.UserType;
 import com.ssafy.ourdoc.global.exception.ForbiddenException;
 
 import lombok.RequiredArgsConstructor;
@@ -28,20 +29,20 @@ public class NotificationHistoryService {
 	private final NotificationRecipientRepository notificationRecipientRepository;
 	private final UserRepository userRepository;
 
-	// 알림 DB 저장
-	public Notification saveHistory(Long senderId, NotificationType type, String content) {
-		User sender = getUser(senderId);
-		// 일단 학생 -> 교사 단방향 알림 보내기 (독서록 작성 시 알림)
-		// Long teacherId = userRepository.findTeacherIdByStudentId(senderId);  // 담당 교사 ID 조회
-		Long teacherId = 9L; // 임시 하드코딩, 학급이 없음
-		User recipient = getUser(teacherId); // 알림 수신자는 본인의 담당교사, class -> teacher_class한 userId
+	// 알림 DB 저장, 일단 학생 -> 담당교사만 보낼 수 있음.
+	public Notification saveHistory(User sender, NotificationType type, String content) {
+		User recipientUser = null;
 
-		if (!isValidNotification(sender, recipient)) {
+		if (sender.getUserType().equals(학생)) {
+			recipientUser = userRepository.findTeachersByStudentClassId(sender.getId());
+		}
+
+		if (!isValidNotification(sender, recipientUser)) {
 			throw new ForbiddenException("허용되지 않은 알림 전송입니다.");
 		}
 
 		Notification notification = saveSender(sender, type, content);
-		saveRecipient(recipient, notification);
+		saveRecipient(recipientUser, notification);
 
 		return notification;
 	}
@@ -65,15 +66,8 @@ public class NotificationHistoryService {
 		notificationRecipientRepository.save(recipientEntity);
 	}
 
-	// 알림 읽음 기록, 누른 알림의 PK ID필요
-	public void markNotificationRead(Long notificationId) {
-		Long userId = 9L; // 현재 로그인 한 사용자 정보 토큰에서 가져오기.
-
+	public void markNotificationRead(User user, Long notificationId) {
 		NotificationRecipient recipient = getRecipient(notificationId);
-
-		if (!recipient.getRecipient().getId().equals(userId)) {
-			throw new ForbiddenException("해당 알림을 읽을 권한이 없습니다.");
-		}
 
 		recipient.markAsRead();
 	}
@@ -87,11 +81,6 @@ public class NotificationHistoryService {
 		}
 
 		return false;  // 기타 경우는 허용하지 않음
-	}
-
-	private User getUser(Long senderId) {
-		return userRepository.findById(senderId)
-			.orElseThrow(() -> new NoSuchElementException("사용자를 찾을 수 없습니다. senderId: " + senderId));
 	}
 
 	private NotificationRecipient getRecipient(Long notificationId) {
