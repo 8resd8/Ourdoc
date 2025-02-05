293a4d92cad6e26979647b77d34bb6e04b6cdae8
김현우 <dkanfjgwls@naver.com>
2025-02-05 17:58:25 +0900

    feat: 학생의 담당교사 찾기

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.ssafy.ourdoc.domain.user.repository;
 
+import static com.ssafy.ourdoc.domain.user.entity.QUser.*;
 import static com.ssafy.ourdoc.domain.user.student.entity.QStudent.*;
 import static com.ssafy.ourdoc.domain.user.teacher.entity.QTeacherClass.*;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ourdoc.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +14,6 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 
 	private final JPAQueryFactory queryFactory;
 
-
 	@Override
 	public Long findTeacherIdByStudentId(Long studentId) {
 		return queryFactory
@@ -20,6 +21,23 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 			.from(student) // StudentClass로 변경해야함.
 			.join(teacherClass).on(student.classRoom.id.eq(teacherClass.classRoom.id))
 			.where(student.user.id.eq(studentId))
+			.fetchOne();
+	}
+
+	// 학생의 담당교사 찾기
+	@Override
+	public User findTeachersByStudentClassId(Long studentUserId) {
+		// 학생의 classId 가져오기
+		Long classId = queryFactory.select(student.classRoom.id)
+			.from(student)
+			.where(student.user.id.eq(studentUserId))
+			.fetchOne();
+
+		// classId로 해당 클래스의 교사 조회
+		return queryFactory.select(user)
+			.from(teacherClass)
+			.join(user).on(teacherClass.user.id.eq(user.id))
+			.where(teacherClass.classRoom.id.eq(classId))
 			.fetchOne();
 	}
 }
