e4bf53911e5476936ba4df6db38d9431d8b86bc3
방승윤 <sybang98@gmail.com>
2025-02-05 17:43:31 +0900

    feat: openvidu 세션 생성 실패 예외클래스 생성

---
+++ backend/src/main/java/com/ssafy/ourdoc/global/integration/openvidu/exception/OpenviduSessionFailException.java
@@ -0,0 +1,5 @@
+package com.ssafy.ourdoc.global.integration.openvidu.exception;
+
+public class OpenviduSessionFailException extends RuntimeException {
+	public OpenviduSessionFailException(String message) { super(message); }
+}
