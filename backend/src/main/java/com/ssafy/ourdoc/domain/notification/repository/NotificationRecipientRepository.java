b9862195f15768f6aa1c04d3abc6984bfebe2bf7
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-05 21:44:20 +0900

    feat: 알림 수신자 정보 반환

--- backend/src/main/java/com/ssafy/ourdoc/domain/notification/repository/NotificationRecipientRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/notification/repository/NotificationRecipientRepository.java
@@ -1,8 +1,14 @@
 package com.ssafy.ourdoc.domain.notification.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ssafy.ourdoc.domain.notification.entity.NotificationRecipient;
 
 public interface NotificationRecipientRepository extends JpaRepository<NotificationRecipient, Long> {
+
+	@Query("select n from NotificationRecipient n where n.recipient = :userId and n.notification.id = :notificationId")
+	Optional<NotificationRecipient> findByNotificationRecipient(Long userId, Long notificationId);
 }
