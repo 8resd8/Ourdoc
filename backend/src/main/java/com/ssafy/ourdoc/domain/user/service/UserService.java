c3ce2b0a85ca3ec00caa1f188938563772352f67
김보민 <mokbee1@naver.com>
2025-02-06 16:19:39 +0900

    rafactor: 학생 로그인

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/service/UserService.java
@@ -1,21 +1,23 @@
 package com.ssafy.ourdoc.domain.user.service;
 
-import java.util.Optional;
+import static com.ssafy.ourdoc.global.common.enums.UserType.*;
+
+import javax.security.auth.login.LoginException;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.ourdoc.domain.user.dto.LoginRequest;
-import com.ssafy.ourdoc.domain.user.dto.LoginResponse;
-import com.ssafy.ourdoc.domain.user.dto.LoginResponse.UserInfo;
-import com.ssafy.ourdoc.domain.user.dto.LoginResult;
 import com.ssafy.ourdoc.domain.user.dto.LogoutResponse;
+import com.ssafy.ourdoc.domain.user.dto.StudentLoginDto;
+import com.ssafy.ourdoc.domain.user.dto.StudentQueryDto;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.domain.user.student.entity.Student;
+import com.ssafy.ourdoc.domain.user.student.repository.StudentQueryRepository;
 import com.ssafy.ourdoc.domain.user.student.repository.StudentRepository;
-import com.ssafy.ourdoc.global.common.enums.TempPassword;
-import com.ssafy.ourdoc.global.common.enums.UserType;
 import com.ssafy.ourdoc.global.config.JwtConfig;
 import com.ssafy.ourdoc.global.exception.UserFailedException;
 import com.ssafy.ourdoc.global.util.JwtBlacklistService;
@@ -34,45 +36,72 @@ public class UserService {
 	private final JwtBlacklistService blacklistService;
 	private final JwtRefreshService refreshService;
 	private final StudentRepository studentRepository;
+	private final StudentQueryRepository studentQueryRepository;
 
 	// 1. 사용자 로그인
-	public LoginResult login(LoginRequest request) {
+	public ResponseEntity<?> login(LoginRequest request) {
+		// 유저 검증 (로그인 중복, 비밀번호)
+		User user = validation(request);
 
-		// 1) 아이디로 User 조회
+		saveRefreshToken(user);
+
+		if (request.userType().equals(학생)) {
+			return loginStudent(request, user);
+		} else if (request.userType().equals(교사)) {
+			// return loginTeacher(request, user);
+		} else {
+
+		}
+		// if 관리자일때
+
+		// 그 외 예외던져
+
+		throw new UserFailedException("알수없는 이유로 로그인 실패");
+	}
+
+	// private ResponseEntity<?> loginTeacher(LoginRequest request, User user) {
+	// }
+
+	private ResponseEntity<StudentLoginDto> loginStudent(LoginRequest request, User user) {
+		Student student = studentRepository.findByUser(user);
+		StudentQueryDto search = studentQueryRepository.getStudentLoginDto(user.getId());
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + getAccessToken(request));
+
+		StudentLoginDto response = new StudentLoginDto(
+			user.getLoginId(),
+			user.getName(),
+			user.getUserType(),
+			search.schoolName(),
+			search.grade(),
+			search.classNumber(),
+			search.studentNumber(),
+			student.getTempPassword());
+
+		return ResponseEntity.ok().headers(headers).body(response);
+	}
+
+	private void saveRefreshToken(User user) {
+		String refreshToken = jwtUtil.createRefreshToken(user.getLoginId());
+		refreshService.storeRefreshToken(user.getLoginId(), refreshToken, jwtConfig.getRefreshExpiration());
+	}
+
+	private User validation(LoginRequest request) {
 		User user = userRepository.findByLoginId(request.loginId())
 			.orElseThrow(() -> new UserFailedException("로그인 실패: 아이디가 존재하지 않습니다."));
 
-		// 2) 비밀번호 해시 검사
 		if (!BCrypt.checkpw(request.password(), user.getPassword())) {
 			throw new UserFailedException("로그인 실패: 비밀번호가 틀렸습니다.");
 		}
 
-		// 3) userType 불일치 시
 		if (!user.getUserType().equals(request.userType())) {
 			throw new UserFailedException("로그인 실패: 유저 타입이 일치하지 않습니다.");
 		}
+		return user;
+	}
 
-		// 4) JWT 토큰 생성
-		String accessToken = jwtUtil.createToken(user.getLoginId(), user.getUserType().toString());
-
-		// 5) refreshToken 생성 및 Redis에 저장
-		String refreshToken = jwtUtil.createRefreshToken(user.getLoginId());
-		refreshService.storeRefreshToken(user.getLoginId(), refreshToken, jwtConfig.getRefreshExpiration());
-
-		// 6) 학생의 경우 tempPassword 추가
-		TempPassword tempPassword = TempPassword.N;
-		if (user.getUserType() == UserType.학생) {
-			tempPassword = studentRepository.findByUser(user).getTempPassword();
-		}
-
-		// 7) 로그인 성공
-		LoginResponse loginResponse = new LoginResponse(
-			"200",
-			"로그인 성공",
-			new UserInfo(user.getLoginId(), user.getName(), user.getUserType().toString(), tempPassword.toString())
-		);
-
-		return new LoginResult(loginResponse, accessToken);
+	public String getAccessToken(LoginRequest request) {
+		return jwtUtil.createToken(request.loginId(), request.userType().toString());
 	}
 
 	// 2. ID 중복 체크
