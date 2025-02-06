785ecb886ba148ad3ce3dbe4305dc8b0da6b1758
윤윤호 <yunho_yun@naver.com>
2025-02-06 10:08:46 +0900

    refactor: 국립중앙도서관 도서 업데이트 코드 분리

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/service/BookService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/service/BookService.java
@@ -2,10 +2,6 @@ package com.ssafy.ourdoc.domain.book.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -15,8 +11,6 @@ import com.ssafy.ourdoc.domain.book.dto.BookRequest;
 import com.ssafy.ourdoc.domain.book.dto.BookResponse;
 import com.ssafy.ourdoc.domain.book.entity.Book;
 import com.ssafy.ourdoc.domain.book.repository.BookRepository;
-import com.ssafy.ourdoc.global.integration.nationallibrary.dto.NationalLibraryBookResponse;
-import com.ssafy.ourdoc.global.integration.nationallibrary.service.NationalLibraryBookService;
 
 import groovy.util.logging.Slf4j;
 import jakarta.transaction.Transactional;
@@ -28,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 @Slf4j
 public class BookService {
 	private final BookRepository bookRepository;
-	private final NationalLibraryBookService nationalLibraryBookService;
 
 	public void registerBook(Book book) {
 		if (bookRepository.findByIsbn(book.getIsbn()).isPresent()) {
@@ -37,41 +30,20 @@ public class BookService {
 		bookRepository.save(book);
 	}
 
+	public void registerBookList(List<Book> books) {
+		bookRepository.saveAll(books);
+	}
+
 	public List<BookResponse> searchBook(BookRequest request) {
 		List<Book> books = bookRepository.findBookList(request.title(), request.author(), request.publisher());
-		if (books.isEmpty()) {
-			updateBookListFromNationalLibrary(request);
-			books = bookRepository.findBookList(request.title(), request.author(), request.publisher());
-		}
-
 		return books.stream()
 			.map(BookResponse::of)
 			.collect(Collectors.toList());
-	}
-
-	public void updateBookListFromNationalLibrary(BookRequest request) {
-		List<NationalLibraryBookResponse> externalBooks = nationalLibraryBookService.parseBook(request);
-		List<NationalLibraryBookResponse> uniqueBooks = externalBooks.stream()
-			.filter(distinctByKey(NationalLibraryBookResponse::isbn))
-			.toList();
-		List<Book> newBooks = uniqueBooks.stream()
-			.filter(response -> bookRepository.findByIsbn(response.isbn()).isEmpty())
-			.map(NationalLibraryBookResponse::toBookEntity)
-			.collect(Collectors.toList());
-
-		if (!newBooks.isEmpty()) {
-			bookRepository.saveAll(newBooks);
-		}
 	}
 
 	public BookDetailResponse getBookDetail(Long id) {
 		Book book = bookRepository.findById(id)
 			.orElseThrow(() -> new NoSuchElementException("해당하는 ID의 도서가 없습니다."));
 		return BookDetailResponse.of(book, book.getDescription());
-	}
-
-	private static <T> Predicate<T> distinctByKey(Function<T, Object> keyExtractor) {
-		Set<Object> seen = ConcurrentHashMap.newKeySet();
-		return t -> seen.add(keyExtractor.apply(t));
 	}
 }
