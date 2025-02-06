743363cfb5fd2abeb0e498ed5d6e3b6ab9456948
김보민 <mokbee1@naver.com>
2025-02-06 17:01:52 +0900

    feat: 교사 로그인 dto 생성

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/dto/TeacherLoginDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.ourdoc.domain.user.dto;
+
+import com.ssafy.ourdoc.global.common.enums.TempPassword;
+import com.ssafy.ourdoc.global.common.enums.UserType;
+
+public record TeacherLoginDto(
+	String loginId,
+	String name,
+	UserType role,
+	String schoolName,
+	int grade,
+	int classNumber
+) {}
