3540bb4f6857a4be2b78544418f6925ea94b4bcf
김보민 <mokbee1@naver.com>
2025-02-06 11:11:41 +0900

    feat: findByUser 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.ourdoc.domain.user.student.repository;
 
+import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    // 필요하다면 custom query 추가
+	Student findByUser(User user);
 }
