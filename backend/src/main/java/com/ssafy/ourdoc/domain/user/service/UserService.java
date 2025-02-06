f2b16dc7f67f03f3477b629b2982bdf8faa7a4e2
김보민 <mokbee1@naver.com>
2025-02-06 20:06:02 +0900

    feat: 비밀번호 일치 여부 확인 기능 생성

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/service/UserService.java
@@ -13,6 +13,7 @@ import com.ssafy.ourdoc.domain.user.dto.StudentLoginDto;
 import com.ssafy.ourdoc.domain.user.dto.StudentQueryDto;
 import com.ssafy.ourdoc.domain.user.dto.TeacherLoginDto;
 import com.ssafy.ourdoc.domain.user.dto.TeacherQueryDto;
+import com.ssafy.ourdoc.domain.user.dto.request.CheckPasswordRequest;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.domain.user.student.entity.Student;
@@ -65,13 +66,8 @@ public class UserService {
 
 		saveRefreshTokenAndSetCookie(user, headers);
 
-		TeacherLoginDto response = new TeacherLoginDto(
-			user.getLoginId(),
-			user.getName(),
-			user.getUserType(),
-			search.schoolName(),
-			search.grade(),
-			search.classNumber());
+		TeacherLoginDto response = new TeacherLoginDto(user.getLoginId(), user.getName(), user.getUserType(),
+			search.schoolName(), search.grade(), search.classNumber());
 
 		return ResponseEntity.ok().headers(headers).body(response);
 	}
@@ -84,14 +80,8 @@ public class UserService {
 
 		saveRefreshTokenAndSetCookie(user, headers);
 
-		StudentLoginDto response = new StudentLoginDto(
-			user.getLoginId(),
-			user.getName(),
-			user.getUserType(),
-			search.schoolName(),
-			search.grade(),
-			search.classNumber(),
-			search.studentNumber(),
+		StudentLoginDto response = new StudentLoginDto(user.getLoginId(), user.getName(), user.getUserType(),
+			search.schoolName(), search.grade(), search.classNumber(), search.studentNumber(),
 			student.getTempPassword());
 
 		return ResponseEntity.ok().headers(headers).body(response);
@@ -142,5 +132,11 @@ public class UserService {
 
 		// 4) 로그아웃 처리 완료
 		return new LogoutResponse("200", "로그아웃 성공");
+	}
+
+	// 비밀번호 일치 여부 확인
+	public boolean verifyPassword(User user, CheckPasswordRequest request) {
+		String encryptedPasswordFromDB = userRepository.findPasswordById(user.getId());
+		return BCrypt.checkpw(request.password(), encryptedPasswordFromDB);
 	}
 }
