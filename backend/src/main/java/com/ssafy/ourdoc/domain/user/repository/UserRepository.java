2ac4229b8e2946f75811d5cbc3119326065147c6
김보민 <mokbee1@naver.com>
2025-02-06 20:05:03 +0900

    feat: findPasswordById 생성

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserQueryRepository {
-    Optional<User> findByLoginIdAndPassword(String loginId, String password);
     Optional<User> findByLoginId(String loginId);
 
+	String findPasswordById(Long id);
 }
