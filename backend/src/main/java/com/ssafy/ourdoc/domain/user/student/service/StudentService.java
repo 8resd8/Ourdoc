87aa35e6981cedbe996787896ef59033ecaf0240
김보민 <mokbee1@naver.com>
2025-02-06 20:54:16 +0900

    refactor: 메서드 분리, active 부분 수정

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/service/StudentService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/service/StudentService.java
@@ -1,5 +1,9 @@
 package com.ssafy.ourdoc.domain.user.student.service;
 
+import static com.ssafy.ourdoc.global.common.enums.Active.*;
+import static com.ssafy.ourdoc.global.common.enums.AuthStatus.*;
+import static com.ssafy.ourdoc.global.common.enums.TempPassword.*;
+
 import java.util.Optional;
 
 import org.mindrot.jbcrypt.BCrypt;
@@ -12,12 +16,14 @@ import com.ssafy.ourdoc.domain.classroom.repository.SchoolRepository;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.domain.user.student.dto.StudentSignupRequest;
+import com.ssafy.ourdoc.domain.user.student.dto.ValidatedEntities;
 import com.ssafy.ourdoc.domain.user.student.entity.Student;
 import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
 import com.ssafy.ourdoc.domain.user.student.repository.StudentClassRepository;
 import com.ssafy.ourdoc.domain.user.student.repository.StudentRepository;
 import com.ssafy.ourdoc.global.common.enums.Active;
 import com.ssafy.ourdoc.global.common.enums.AuthStatus;
+import com.ssafy.ourdoc.global.common.enums.TempPassword;
 import com.ssafy.ourdoc.global.common.enums.UserType;
 
 import jakarta.transaction.Transactional;
@@ -37,9 +43,45 @@ public class StudentService {
 	// 1. 학생 회원가입
 	public Long signup(StudentSignupRequest request) {
 
+		ValidatedEntities validatedEntities = validateAndRetrieveEntities(request);
+
+		// User 엔티티 생성
+		User user = User.builder()
+			.userType(UserType.학생)
+			.name(request.name())
+			.loginId(request.loginId())
+			.password(validatedEntities.encodedPassword())
+			.birth(request.birth())
+			.gender(request.gender())
+			.active(활성)
+			.build();
+		User savedUser = userRepository.save(user);
+
+		// Student 엔티티 생성
+		Student student = Student.builder()
+			.user(savedUser)
+			.classRoom(validatedEntities.classRoom())
+			.authStatus(대기)
+			.tempPassword(N)
+			.build();
+		Student savedStudent = studentRepository.save(student);
+
+		// StudentClass 엔티티 생성
+		StudentClass studentClass = StudentClass.builder()
+			.user(savedUser)
+			.classRoom(validatedEntities.classRoom())
+			.studentNumber(request.studentNumber())
+			.active(활성)
+			.authStatus(대기)
+			.build();
+		studentClassRepository.save(studentClass);
+
+		return savedStudent.getId();
+	}
+
+	public ValidatedEntities validateAndRetrieveEntities(StudentSignupRequest request) {
 		// 1) 아이디 중복 체크
-		Optional<User> existingUser = userRepository.findByLoginId(request.loginId());
-		if (existingUser.isPresent()) {
+		if (userRepository.findByLoginId(request.loginId()).isPresent()) {
 			throw new IllegalArgumentException("이미 존재하는 로그인 ID입니다.");
 		}
 
@@ -55,35 +97,6 @@ public class StudentService {
 			school, request.grade(), request.classNumber()
 		).orElseThrow(() -> new IllegalArgumentException("해당 학년 및 반 정보를 찾을 수 없습니다."));
 
-		// 5) User 엔티티 생성
-		User user = User.builder()
-			.userType(UserType.학생)
-			.name(request.name())
-			.loginId(request.loginId())
-			.password(encodedPassword)
-			.birth(request.birth())
-			.gender(request.gender())
-			.active(Active.활성)
-			.build();
-		User savedUser = userRepository.save(user);
-
-		// 6) Student 엔티티 생성
-		Student student = Student.builder()
-			.user(savedUser)
-			.classRoom(classRoom)
-			.build();
-		Student savedStudent = studentRepository.save(student);
-
-		// 7) StudentClass 엔티티 생성
-		StudentClass studentClass = StudentClass.builder()
-			.user(savedUser)
-			.classRoom(classRoom)
-			.studentNumber(request.studentNumber())
-			.active(request.active())
-			.authStatus(AuthStatus.대기)
-			.build();
-		studentClassRepository.save(studentClass);
-
-		return savedStudent.getId();
+		return new ValidatedEntities(encodedPassword, school, classRoom);
 	}
 }
