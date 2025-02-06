2429ee54c4a0b1c4d6033f1a26fad33814955868
김현우 <dkanfjgwls@naver.com>
2025-02-06 17:15:49 +0900

    feat: 학급학생으로 독서록 조회

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.ourdoc.domain.bookreport.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.bookreport.entity.BookReport;
 
-public interface BookReportRepository extends JpaRepository<BookReport, Long> {
+public interface BookReportRepository extends JpaRepository<BookReport, Long>, BookReportQueryRepository {
+	List<BookReport> findByStudentClassId(Long studentClassId);
 }
