ca3b86210e8a770602312415355a11a4a871fdda
김현우 <dkanfjgwls@naver.com>
2025-02-06 17:21:19 +0900

    feat: 독서록 조회 컨트롤러

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportStudentController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportStudentController.java
@@ -1,6 +1,7 @@
 package com.ssafy.ourdoc.domain.bookreport.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReadLogRequest;
+import com.ssafy.ourdoc.domain.bookreport.dto.BookReportListResponse;
 import com.ssafy.ourdoc.domain.bookreport.dto.FeedbackRequest;
 import com.ssafy.ourdoc.domain.bookreport.service.BookReportStudentService;
 import com.ssafy.ourdoc.domain.user.entity.User;
@@ -32,6 +34,11 @@ public class BookReportStudentController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createBookReportFeedback(@RequestBody FeedbackRequest request) {
 		bookReportStudentService.saveBookReportFeedback(request);
+	}
+
+	@GetMapping
+	public BookReportListResponse getBookReportList(@Login User user) {
+		return bookReportStudentService.getBookReports(user);
 	}
 
 }
