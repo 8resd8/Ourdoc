d67e6fa198457e8c4c01663977b947ff42961ab8
방승윤 <sybang98@gmail.com>
2025-02-07 14:59:10 +0900

    feat: 독서토론방 목록 조회

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
@@ -1,7 +1,10 @@
 package com.ssafy.ourdoc.domain.debate.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.debate.dto.CreateRoomRequest;
 import com.ssafy.ourdoc.domain.debate.dto.JoinRoomRequest;
+import com.ssafy.ourdoc.domain.debate.dto.RoomDto;
 import com.ssafy.ourdoc.domain.debate.service.DebateService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.annotation.Login;
@@ -24,6 +28,12 @@ public class DebateController {
 
 	private final DebateService debateService;
 
+	@GetMapping
+	public ResponseEntity<List<RoomDto>> getDebateRooms() {
+		List<RoomDto> rooms = debateService.getDebateRooms();
+		return ResponseEntity.ok(rooms);
+	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createDebateRoom(@Login User user, @RequestBody CreateRoomRequest request) {
@@ -32,7 +42,7 @@ public class DebateController {
 
 	@PostMapping("/{roomId}/connection")
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<String> joinDebateRoom(@Login User user, @PathVariable Long roomId, @RequestBody JoinRoomRequest request) {
+	public ResponseEntity<String> joinDebateRoom(@Login User user, @PathVariable("roomId") Long roomId, @RequestBody JoinRoomRequest request) {
 		String token = debateService.joinDebateRoom(user, roomId, request);
 		return ResponseEntity.ok(token);
 	}
