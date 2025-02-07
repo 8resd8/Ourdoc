1feab33d7777efc3c6c5282e4cadfa5490b77e94
방승윤 <sybang98@gmail.com>
2025-02-07 09:41:36 +0900

    feat: 독서토론방 접속 인원

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/RoomOnlineRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/RoomOnlineRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 public interface RoomOnlineRepository extends JpaRepository<RoomOnline, Long> {
+	int countByRoomIdAndUpdatedAtNull(Long roomId);
 }
