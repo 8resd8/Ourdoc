000505a2e407996b650301cf8c3e65d74eea450c
윤윤호 <yunho_yun@naver.com>
2025-02-07 15:53:45 +0900

    feat: 학급 내 학생 수 조회 repo 정의

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.ourdoc.domain.classroom.entity.ClassRoom;
 import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
 import com.ssafy.ourdoc.global.common.enums.Active;
 
@@ -11,4 +12,6 @@ public interface StudentClassRepository extends JpaRepository<StudentClass, Long
 	Optional<StudentClass> findStudentClassByUserId(Long userId);
 
 	StudentClass findByUserIdAndActive(Long userId, Active active);
+
+	int countByClassRoom(ClassRoom userClassRoom);
 }
