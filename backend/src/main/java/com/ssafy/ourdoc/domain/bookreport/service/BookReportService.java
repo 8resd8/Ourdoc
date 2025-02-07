0cb12a6f8897fd1de52265b4049b7acc7dd72921
김현우 <dkanfjgwls@naver.com>
2025-02-07 14:47:47 +0900

    feat: 독서록 상세보기 서비스코드

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/service/BookReportService.java
@@ -0,0 +1,43 @@
+package com.ssafy.ourdoc.domain.bookreport.service;
+
+import static com.ssafy.ourdoc.global.common.enums.ApproveStatus.*;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ourdoc.domain.bookreport.dto.BookReportDetailDto;
+import com.ssafy.ourdoc.domain.bookreport.dto.BookReportDetailResponse;
+import com.ssafy.ourdoc.domain.bookreport.repository.BookReportRepository;
+import com.ssafy.ourdoc.global.common.enums.ApproveStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookReportService {
+
+	private final BookReportRepository bookReportRepository;
+
+	public BookReportDetailResponse getBookReportDetail(Long bookReportId) {
+		BookReportDetailDto detailDto = bookReportRepository.bookReportDetail(bookReportId);
+		if (detailDto == null) {
+			throw new NoSuchElementException("해당 독서록을 찾을 수 없습니다.");
+		}
+
+		ApproveStatus approveStatus = (detailDto.approveTime() != null) ? 있음 : 없음;
+		String aiComment = detailDto.aiComment() == null ? "없음" : detailDto.aiComment();
+		String teacherComment = detailDto.teacherComment() == null ? "없음" : detailDto.teacherComment();
+
+		return new BookReportDetailResponse(
+			detailDto.bookTitle(),
+			detailDto.author(),
+			detailDto.publisher(),
+			detailDto.createdAt(),
+			detailDto.content(),
+			aiComment,
+			teacherComment,
+			approveStatus
+		);
+	}
+}
