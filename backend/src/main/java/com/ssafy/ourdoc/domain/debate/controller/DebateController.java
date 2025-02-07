8a39b841f68d6dbcc4c9fb7c4ad096b7ed94aa2a
방승윤 <sybang98@gmail.com>
2025-02-07 09:39:55 +0900

    feat: 독서토론방 참가

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
@@ -1,6 +1,8 @@
 package com.ssafy.ourdoc.domain.debate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.debate.dto.CreateRoomRequest;
+import com.ssafy.ourdoc.domain.debate.dto.JoinRoomRequest;
 import com.ssafy.ourdoc.domain.debate.service.DebateService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.annotation.Login;
@@ -25,5 +28,12 @@ public class DebateController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createDebateRoom(@Login User user, @RequestBody CreateRoomRequest request) {
 		debateService.createDebateRoom(user, request);
+	}
+
+	@PostMapping("/{roomId}/connection")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> joinDebateRoom(@Login User user, @PathVariable Long roomId, @RequestBody JoinRoomRequest request) {
+		String token = debateService.joinDebateRoom(user, roomId, request);
+		return ResponseEntity.ok(token);
 	}
 }
