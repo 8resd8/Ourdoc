4f6ad18d9fe31ffc31785bf4c089564ed05b0f41
윤윤호 <yunho_yun@naver.com>
2025-02-07 12:16:29 +0900

    feat: 학생 학급 repo에 userId, active 검색 기능 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
+import com.ssafy.ourdoc.global.common.enums.Active;
 
 public interface StudentClassRepository extends JpaRepository<StudentClass, Long> {
 	Optional<StudentClass> findStudentClassByUserId(Long userId);
+
+	StudentClass findByUserIdAndActive(Long userId, Active active);
 }
