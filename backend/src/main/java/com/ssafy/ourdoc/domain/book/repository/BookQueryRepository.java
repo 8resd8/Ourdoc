add9593afcce6d63258433a14e844f3d8a15a5c4
윤윤호 <yunho_yun@naver.com>
2025-02-07 14:42:45 +0900

    fix: 추천도서 조회 로직 분리
    - join 연산을 위해 올바른 repository에서 동작하도록 로직 분리

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookQueryRepository.java
@@ -3,11 +3,7 @@ package com.ssafy.ourdoc.domain.book.repository;
 import java.util.List;
 
 import com.ssafy.ourdoc.domain.book.entity.Book;
-import com.ssafy.ourdoc.domain.book.entity.BookRecommend;
-import com.ssafy.ourdoc.domain.user.entity.User;
 
 public interface BookQueryRepository {
 	List<Book> findBookList(String title, String author, String publisher);
-
-	List<BookRecommend> findRecommendBookList(User user);
 }
