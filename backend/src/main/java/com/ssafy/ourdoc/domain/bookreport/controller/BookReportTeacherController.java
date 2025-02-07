e36b15671c4c30b10b32ec18738c377a212469af
김현우 <dkanfjgwls@naver.com>
2025-02-07 14:46:58 +0900

    feat: 교사 독서록 전체조회 컨트롤러

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
@@ -0,0 +1,26 @@
+package com.ssafy.ourdoc.domain.bookreport.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherListResponse;
+import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherRequest;
+import com.ssafy.ourdoc.domain.bookreport.service.BookReportTeacherService;
+import com.ssafy.ourdoc.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("bookreports")
+public class BookReportTeacherController {
+
+	private final BookReportTeacherService bookReportTeacherService;
+
+	@GetMapping("/teacher")
+	public ReportTeacherListResponse getBookReportList(User user, @RequestBody ReportTeacherRequest request) {
+		return bookReportTeacherService.getBookReports(user, request);
+	}
+}
