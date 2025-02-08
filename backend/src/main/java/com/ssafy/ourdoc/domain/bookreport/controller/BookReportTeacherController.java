3d27f35291068766de32111e83c7002c9fa3ad59
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-08 16:31:20 +0900

    rebase commit

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
@@ -1,7 +1,12 @@
 package com.ssafy.ourdoc.domain.bookreport.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportCommentRequest;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherListResponse;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherRequest;
 import com.ssafy.ourdoc.domain.bookreport.service.BookReportTeacherService;
@@ -19,12 +25,12 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("bookreports/teacher")
+@RequestMapping("/bookreports/teachers")
 public class BookReportTeacherController {
 
 	private final BookReportTeacherService bookReportTeacherService;
 
-	@GetMapping("/teacher")
+	@GetMapping
 	public ReportTeacherListResponse getBookReportList(@Login User user, @RequestBody ReportTeacherRequest request) {
 		return bookReportTeacherService.getBookReports(user, request);
 	}
@@ -34,4 +40,25 @@ public class BookReportTeacherController {
 	public void approveBookReport(@Login User user, @PathVariable("bookReportId") Long bookReportId) {
 		bookReportTeacherService.approveStamp(user, bookReportId);
 	}
+
+	@PostMapping("/{bookreportId}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createBookReportComment(@Login User user, @PathVariable("bookreportId") Long bookreportId,
+		@RequestBody ReportCommentRequest request) {
+		bookReportTeacherService.createComment(user, bookreportId, request);
+	}
+
+	@PatchMapping("/{bookreportId}")
+	@ResponseStatus(HttpStatus.OK)
+	public void updateBookReportComment(@PathVariable("bookreportId") Long bookreportId,
+		@RequestBody ReportCommentRequest request) {
+		bookReportTeacherService.updateComment(bookreportId, request);
+	}
+
+	@DeleteMapping("/{bookreportId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteBookReportComment(@PathVariable("bookreportId") Long bookreportId) {
+		bookReportTeacherService.deleteComment(bookreportId);
+	}
+
 }
