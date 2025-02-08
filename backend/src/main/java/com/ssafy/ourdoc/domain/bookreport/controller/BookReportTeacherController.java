06c181c53af771552993c557a13adef89f43891e
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-08 12:40:42 +0900

    featL: 교사가 학생 독서록에 칭잔도장 매핑 컨트롤러

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
@@ -1,8 +1,12 @@
 package com.ssafy.ourdoc.domain.bookreport.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherListResponse;
@@ -15,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("bookreports")
+@RequestMapping("bookreports/teacher")
 public class BookReportTeacherController {
 
 	private final BookReportTeacherService bookReportTeacherService;
@@ -23,5 +27,11 @@ public class BookReportTeacherController {
 	@GetMapping("/teacher")
 	public ReportTeacherListResponse getBookReportList(@Login User user, @RequestBody ReportTeacherRequest request) {
 		return bookReportTeacherService.getBookReports(user, request);
+	}
+
+	@PatchMapping("/{bookReportId}/stamp")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void approveBookReport(@Login User user, @PathVariable("bookReportId") Long bookReportId) {
+		bookReportTeacherService.approveStamp(user, bookReportId);
 	}
 }
