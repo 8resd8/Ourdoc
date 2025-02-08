c7dad74850c9c99fb5def863326403448c0d5726
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-08 16:38:07 +0900

    refactor: api 명세서에 맞도록 경로 조정

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportTeacherController.java
@@ -2,13 +2,10 @@ package com.ssafy.ourdoc.domain.bookreport.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,21 +38,21 @@ public class BookReportTeacherController {
 		bookReportTeacherService.approveStamp(user, bookReportId);
 	}
 
-	@PostMapping("/{bookreportId}")
+	@PostMapping("/{bookreportId}/comment")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createBookReportComment(@Login User user, @PathVariable("bookreportId") Long bookreportId,
 		@RequestBody ReportCommentRequest request) {
 		bookReportTeacherService.createComment(user, bookreportId, request);
 	}
 
-	@PatchMapping("/{bookreportId}")
+	@PatchMapping("/{bookreportId}/comment")
 	@ResponseStatus(HttpStatus.OK)
 	public void updateBookReportComment(@PathVariable("bookreportId") Long bookreportId,
 		@RequestBody ReportCommentRequest request) {
 		bookReportTeacherService.updateComment(bookreportId, request);
 	}
 
-	@DeleteMapping("/{bookreportId}")
+	@DeleteMapping("/{bookreportId}/comment")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteBookReportComment(@PathVariable("bookreportId") Long bookreportId) {
 		bookReportTeacherService.deleteComment(bookreportId);
