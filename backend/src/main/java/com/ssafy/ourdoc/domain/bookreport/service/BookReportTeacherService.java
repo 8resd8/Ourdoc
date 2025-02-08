801bb63b9a544a040999d448f113b304dac98af9
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-08 16:28:13 +0900

    feat: 의견 작성 수정 삭제로직 서비스로직 구현

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/service/BookReportTeacherService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/service/BookReportTeacherService.java
@@ -5,28 +5,40 @@ import static com.ssafy.ourdoc.global.common.enums.NotificationType.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportCommentRequest;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherListResponse;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherRequest;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherResponse;
 import com.ssafy.ourdoc.domain.bookreport.entity.BookReport;
+import com.ssafy.ourdoc.domain.bookreport.entity.BookReport;
+import com.ssafy.ourdoc.domain.bookreport.entity.BookReportFeedBack;
+import com.ssafy.ourdoc.domain.bookreport.repository.BookReportFeedbackRepository;
 import com.ssafy.ourdoc.domain.bookreport.repository.BookReportRepository;
 import com.ssafy.ourdoc.domain.notification.service.NotificationService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
 import com.ssafy.ourdoc.global.common.enums.NotificationType;
+import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
+import com.ssafy.ourdoc.domain.user.teacher.repository.TeacherClassRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BookReportTeacherService {
 
 	private final BookReportRepository bookReportRepository;
 	private final NotificationService notificationService;
+	private final BookReportFeedbackRepository bookReportFeedbackRepository;
+	private final NotificationService notificationService;
+	private final TeacherClassRepository teacherClassRepository;
 
 	public ReportTeacherListResponse getBookReports(User user, ReportTeacherRequest request) {
 		List<ReportTeacherResponse> convertDto = getReportTeacherResponses(
@@ -56,5 +68,30 @@ public class BookReportTeacherService {
 
 		StudentClass studentClass = bookReport.getStudentClass();
 		notificationService.sendNotifyTeacherFromStudent(user, studentClass.getId());
+	}
+
+	public void createComment(User user, Long bookReportId, ReportCommentRequest request) {
+		BookReportFeedBack bookReportFeedBack = getBookReport(bookReportId).getBookReportFeedBack();
+
+		StudentClass studentClass = getBookReport(bookReportId).getStudentClass();
+
+		bookReportFeedBack.updateComment(request.comment());
+		notificationService.sendNotifyTeacherFromStudent(user, studentClass.getId());
+	}
+
+	public void updateComment(Long bookReportId, ReportCommentRequest request) {
+		BookReportFeedBack bookReportFeedBack = getBookReport(bookReportId).getBookReportFeedBack();
+		bookReportFeedBack.updateComment(request.comment());
+	}
+
+	public void deleteComment(Long bookReportId) {
+		BookReportFeedBack bookReportFeedBack = getBookReport(bookReportId).getBookReportFeedBack();
+
+		bookReportFeedBack.updateComment(null);
+	}
+
+	private BookReport getBookReport(Long bookReportId) {
+		return bookReportRepository.findById(bookReportId)
+			.orElseThrow(() -> new NoSuchElementException("도장찍을 독서록이 없습니다."));
 	}
 }
