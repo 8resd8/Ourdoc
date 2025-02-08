87227ef971fb98144bda33aa0e96c1a1d4efb228
방승윤 <sybang98@gmail.com>
2025-02-08 18:18:41 +0900

    feat: 독서토론방 접속 여부

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomOnlineRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomOnlineRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.ourdoc.domain.debate.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 public interface DebateRoomOnlineRepository extends JpaRepository<RoomOnline, Long>, DebateRoomQueryRepository {
+	Optional<RoomOnline> findByRoom_IdAndUser_IdAndAndUpdatedAtIsNull(Long roomId, Long userId);
 }
