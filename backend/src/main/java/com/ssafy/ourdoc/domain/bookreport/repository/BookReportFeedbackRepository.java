451ba20952d29af40100c54579a1a28c828251d4
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-08 16:03:39 +0900

    feat: bookId로 feedbackBook 조회

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportFeedbackRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportFeedbackRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.ourdoc.domain.bookreport.entity.BookReportFeedBack;
 
 public interface BookReportFeedbackRepository extends JpaRepository<BookReportFeedBack, Long> {
+	BookReportFeedBack findByBookReportId(Long bookReportId);
 }
