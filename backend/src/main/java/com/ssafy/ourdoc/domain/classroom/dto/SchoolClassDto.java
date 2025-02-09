effc4809b8b82780f6ec620de5098fa1b6c66967
윤윤호 <yunho_yun@naver.com>
2025-02-10 01:00:54 +0900

    feat: 학급-학교 정보 dto 정의

---
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/dto/SchoolClassDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.ourdoc.domain.classroom.dto;
+
+import java.time.Year;
+
+import com.querydsl.core.annotations.QueryProjection;
+
+public record SchoolClassDto(
+	String schoolName,
+	int grade,
+	int classNumber,
+	Year year,
+	int studentCount
+) {
+	@QueryProjection
+	public SchoolClassDto {
+	}
+}
