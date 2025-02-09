53f47f9ec6d3b07388e2af64496a46a1064432db
방승윤 <sybang98@gmail.com>
2025-02-09 21:58:35 +0900

    refactor: 접속중인지 확인하는 JPA 쿼리 메소드

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
@@ -2,7 +2,10 @@ package com.ssafy.ourdoc.domain.debate.repository;
 
 import static com.ssafy.ourdoc.domain.debate.entity.QRoomOnline.*;
 
+import java.util.Optional;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,15 @@ public class DebateRoomQueryRepositoryImpl implements DebateRoomQueryRepository 
 				roomOnline.room.id.eq(roomId)
 					.and(roomOnline.createdAt.eq(roomOnline.updatedAt))
 			).fetchOne();
+	}
+
+	public Optional<RoomOnline> findActiveByRoomIdAndUserId(Long roomId, Long userId) {
+		RoomOnline result = queryFactory.selectFrom(roomOnline)
+			.where(
+				roomOnline.room.id.eq(roomId),
+				roomOnline.user.id.eq(userId),
+				roomOnline.createdAt.eq(roomOnline.updatedAt)
+			).fetchOne();
+		return Optional.ofNullable(result);
 	}
 }
