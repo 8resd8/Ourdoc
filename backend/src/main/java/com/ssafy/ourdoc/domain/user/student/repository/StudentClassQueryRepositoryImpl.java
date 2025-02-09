68f8a8e97a8ecc9735211ed7b53183616248a7be
김보민 <mokbee1@naver.com>
2025-02-09 18:03:00 +0900

    feat: student 테이블에 authState 업데이트

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.ourdoc.domain.user.student.repository;
 
+import static com.ssafy.ourdoc.domain.user.student.entity.QStudent.*;
 import static com.ssafy.ourdoc.domain.user.student.entity.QStudentClass.*;
 import static com.ssafy.ourdoc.global.common.enums.AuthStatus.*;
 
@@ -17,7 +18,7 @@ public class StudentClassQueryRepositoryImpl implements StudentClassQueryReposit
 	private final JPAQueryFactory queryFactory;
 
 	@Override
-	public long updateAuthStatus(Long userId, Long classId, AuthStatus newStatus) {
+	public long updateAuthStatusOfStudentClass(Long userId, Long classId, AuthStatus newStatus) {
 		return queryFactory
 			.update(studentClass)
 			.set(studentClass.authStatus, newStatus)
@@ -25,6 +26,18 @@ public class StudentClassQueryRepositoryImpl implements StudentClassQueryReposit
 				studentClass.user.id.eq(userId),
 				studentClass.classRoom.id.eq(classId),
 				studentClass.authStatus.eq(대기)
+			)
+			.execute();
+	}
+
+	@Override
+	public long updateAuthStatusOfStudent(Long userId, AuthStatus newStatus) {
+		return queryFactory
+			.update(student)
+			.set(student.authStatus, newStatus)
+			.where(
+				student.user.id.eq(userId),
+				student.authStatus.eq(대기)
 			)
 			.execute();
 	}
