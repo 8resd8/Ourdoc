df327db94cbf1e5b3ae6dc85b7d28f9addeee0e6
윤윤호 <yunho_yun@naver.com>
2025-02-10 00:40:13 +0900

    feat: 교사 숙제 상세 조회 컨트롤러 구현

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/HomeworkController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/HomeworkController.java
@@ -1,12 +1,16 @@
 package com.ssafy.ourdoc.domain.book.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.book.dto.BookRequest;
+import com.ssafy.ourdoc.domain.book.dto.homework.HomeworkDetailTeacher;
 import com.ssafy.ourdoc.domain.book.service.HomeworkService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.annotation.Login;
@@ -28,5 +32,10 @@ public class HomeworkController {
 	@DeleteMapping("/teachers/homework")
 	public void deleteHomework(@RequestBody BookRequest request, @Login User user) {
 		homeworkService.deleteHomework(request, user);
+	}
+
+	@GetMapping("/teachers/homework/{homeworkId}")
+	public ResponseEntity<HomeworkDetailTeacher> getHomework(@PathVariable long homeworkId, @Login User user) {
+		return ResponseEntity.ok(homeworkService.getHomeworkDetailTeacher(homeworkId, user));
 	}
 }
