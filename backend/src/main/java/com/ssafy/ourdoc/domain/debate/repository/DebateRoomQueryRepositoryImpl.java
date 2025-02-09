3e252be13dc8077471be470981c1aad625f7d91b
방승윤 <sybang98@gmail.com>
2025-02-09 22:39:20 +0900

    feat: 독서토론방 접속중인 사람 목록

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
@@ -2,7 +2,10 @@ package com.ssafy.ourdoc.domain.debate.repository;
 
 import static com.ssafy.ourdoc.domain.debate.entity.QRoomOnline.*;
 
+import java.util.List;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,13 @@ public class DebateRoomQueryRepositoryImpl implements DebateRoomQueryRepository 
 				roomOnline.room.id.eq(roomId)
 					.and(roomOnline.createdAt.eq(roomOnline.updatedAt))
 			).fetchOne();
+	}
+
+	public List<RoomOnline> findAllActiveByRoomId(Long roomId) {
+		return queryFactory.selectFrom(roomOnline)
+			.where(
+				roomOnline.room.id.eq(roomId)
+					.and(roomOnline.createdAt.eq(roomOnline.updatedAt))
+			).fetch();
 	}
 }
