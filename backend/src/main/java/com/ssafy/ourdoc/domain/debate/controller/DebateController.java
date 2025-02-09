7a17c9ef9a6c0a8bb0ffc9279835f555030b9664
방승윤 <sybang98@gmail.com>
2025-02-10 00:02:51 +0900

    feat: 독서토론방 상세 조회 컨트롤러

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/controller/DebateController.java
@@ -1,7 +1,5 @@
 package com.ssafy.ourdoc.domain.debate.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -16,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.debate.dto.CreateRoomRequest;
 import com.ssafy.ourdoc.domain.debate.dto.JoinRoomRequest;
+import com.ssafy.ourdoc.domain.debate.dto.RoomDetailDto;
 import com.ssafy.ourdoc.domain.debate.dto.RoomDto;
 import com.ssafy.ourdoc.domain.debate.service.DebateService;
 import com.ssafy.ourdoc.domain.user.entity.User;
@@ -47,5 +46,12 @@ public class DebateController {
 	public ResponseEntity<String> joinDebateRoom(@Login User user, @PathVariable("roomId") Long roomId, @RequestBody JoinRoomRequest request) {
 		String token = debateService.joinDebateRoom(user, roomId, request);
 		return ResponseEntity.ok(token);
+	}
+
+	@GetMapping("/{roomId}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<RoomDetailDto> getDebateRoom(@PathVariable("roomId") Long roomId) {
+		RoomDetailDto roomDetailDto = debateService.getDebateRoomDetail(roomId);
+		return ResponseEntity.ok(roomDetailDto);
 	}
 }
