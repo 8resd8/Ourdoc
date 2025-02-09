b106d63d9a83da2575293a02184fa54a0dc3530b
김보민 <mokbee1@naver.com>
2025-02-09 18:02:50 +0900

    feat: student 테이블에 authState 업데이트

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
@@ -3,5 +3,7 @@ package com.ssafy.ourdoc.domain.user.student.repository;
 import com.ssafy.ourdoc.global.common.enums.AuthStatus;
 
 public interface StudentClassQueryRepository {
-	long updateAuthStatus(Long userId, Long classId, AuthStatus newStatus);
+	long updateAuthStatusOfStudentClass(Long userId, Long classId, AuthStatus newStatus);
+
+	long updateAuthStatusOfStudent(Long id, AuthStatus authStatus);
 }
