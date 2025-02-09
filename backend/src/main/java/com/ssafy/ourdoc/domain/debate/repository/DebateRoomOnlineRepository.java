14d013ae5f1997958dfc90882f53407983ca36f0
방승윤 <sybang98@gmail.com>
2025-02-09 21:58:26 +0900

    refactor: 접속중인지 확인하는 JPA 쿼리 메소드

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomOnlineRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomOnlineRepository.java
@@ -1,11 +1,8 @@
 package com.ssafy.ourdoc.domain.debate.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 public interface DebateRoomOnlineRepository extends JpaRepository<RoomOnline, Long>, DebateRoomQueryRepository {
-	Optional<RoomOnline> findByRoomIdAndUserIdAndCreatedAtEqualsUpdatedAt(Long roomId, Long userId);
 }
