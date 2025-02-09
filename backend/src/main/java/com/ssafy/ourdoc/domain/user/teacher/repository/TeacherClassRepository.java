a05f6a0b8923d6ef97a203e7c6a47f66d4b6e2be
김보민 <mokbee1@naver.com>
2025-02-09 17:14:40 +0900

    feat: 교사가 소속된 학급 찾기

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherClassRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherClassRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.teacher.entity.TeacherClass;
 import com.ssafy.ourdoc.global.common.enums.Active;
 
@@ -11,4 +12,6 @@ public interface TeacherClassRepository extends JpaRepository<TeacherClass, Long
 	Optional<TeacherClass> findByUserIdAndClassRoomId(Long userId, Long classRoomId);
 
 	Optional<TeacherClass> findByUserIdAndActive(Long userId, Active active);
+
+	TeacherClass findClassRoomByUserAndActive(User user, Active active);
 }
