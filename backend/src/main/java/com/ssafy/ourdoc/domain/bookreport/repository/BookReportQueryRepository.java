d0ce6bdfe435ca5f812472bc698da02161f6fde9
윤윤호 <yunho_yun@naver.com>
2025-02-10 00:36:51 +0900

    feat: 숙제에 해당하는 독서록 목록 조회 쿼리 구현

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportQueryRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportDetailDto;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherDto;
+import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherDtoWithId;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherRequest;
 
 public interface BookReportQueryRepository {
@@ -11,4 +12,6 @@ public interface BookReportQueryRepository {
 	List<ReportTeacherDto> bookReports(Long userId, ReportTeacherRequest request);
 
 	BookReportDetailDto bookReportDetail(Long reportId);
+
+	List<ReportTeacherDtoWithId> bookReportsHomework(Long homeworkId);
 }
