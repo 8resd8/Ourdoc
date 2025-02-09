0dd965bce89bdb781e9f8038355e7883f8b22001
김보민 <mokbee1@naver.com>
2025-02-09 20:54:39 +0900

    feat: 본인 학급 학생 목록 조회 기능 생성

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/controller/TeacherController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/controller/TeacherController.java
@@ -1,21 +1,23 @@
 package com.ssafy.ourdoc.domain.user.teacher.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.ourdoc.domain.user.entity.User;
+import com.ssafy.ourdoc.domain.user.teacher.dto.StudentListResponse;
 import com.ssafy.ourdoc.domain.user.teacher.dto.TeacherSignupRequest;
 import com.ssafy.ourdoc.domain.user.teacher.service.TeacherService;
+import com.ssafy.ourdoc.global.annotation.Login;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/teachers")
@@ -36,5 +38,11 @@ public class TeacherController {
 	public ResponseEntity<byte[]> generateTeacherInviteCode(@PathVariable Long teacherId) {
 		byte[] qrImage = teacherService.generateTeacherClassQr(teacherId);
 		return ResponseEntity.ok(qrImage);
+	}
+
+	// 본인 학급 학생 목록 조회
+	@GetMapping("/students/profile")
+	public StudentListResponse getStudentList(@Login User user) {
+		return teacherService.getMyClassStudentList(user);
 	}
 }
