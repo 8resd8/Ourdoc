79df02a680aec07658ea630746ab32f4a4c03fe6
방승윤 <sybang98@gmail.com>
2025-02-09 22:39:17 +0900

    feat: 독서토론방 접속중인 사람 목록

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepository.java
@@ -1,5 +1,10 @@
 package com.ssafy.ourdoc.domain.debate.repository;
 
+import java.util.List;
+
+import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
+
 public interface DebateRoomQueryRepository {
 	Long countCurrentPeople(Long roomId);
+	List<RoomOnline> findAllActiveByRoomId(Long roomId);
 }
