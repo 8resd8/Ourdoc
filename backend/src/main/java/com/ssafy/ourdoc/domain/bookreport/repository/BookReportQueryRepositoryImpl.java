c5b3277d2f401e1fa78ac01fd3b573800ebe5a4a
김현우 <dkanfjgwls@naver.com>
2025-02-10 13:02:32 +0900

    feat: 페이지네이션 쿼리 적용

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/repository/BookReportQueryRepositoryImpl.java
@@ -12,7 +12,12 @@ import static com.ssafy.ourdoc.global.common.enums.EvaluatorType.*;
 import java.time.Year;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportDetailDto;
 import com.ssafy.ourdoc.domain.bookreport.dto.QBookReportDetailDto;
@@ -29,7 +34,7 @@ public class BookReportQueryRepositoryImpl implements BookReportQueryRepository 
 	private final JPAQueryFactory queryFactory;
 
 	@Override
-	public List<ReportTeacherDto> bookReports(Long userId, ReportTeacherRequest request) {
+	public Page<ReportTeacherDto> bookReports(Long userId, ReportTeacherRequest request, Pageable pageable) {
 		// 교사가 지금까지 했던 반 정보
 		List<Long> teacherContainClass = queryFactory
 			.select(teacherClass.classRoom.id)
@@ -37,7 +42,7 @@ public class BookReportQueryRepositoryImpl implements BookReportQueryRepository 
 			.where(teacherClass.user.id.eq(userId))
 			.fetch();
 
-		return queryFactory
+		List<ReportTeacherDto> content = queryFactory
 			.select(new QReportTeacherDto(
 				book.title,
 				studentClass.studentNumber,
@@ -57,7 +62,30 @@ public class BookReportQueryRepositoryImpl implements BookReportQueryRepository 
 				eqStudentNumber(request.studentNumber()),
 				containsStudentName(request.studentName()),
 				eqSchoolName(request.schoolName())
-			).fetch();
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		long total = getTotal(request, teacherContainClass)
+			.fetchOne();
+
+		return new PageImpl<>(content, pageable, total);
+	}
+
+	private JPAQuery<Long> getTotal(ReportTeacherRequest request, List<Long> teacherContainClass) {
+		return queryFactory
+			.select(bookReport.count())
+			.from(bookReport)
+			.join(bookReport.studentClass, studentClass)
+			.join(studentClass.classRoom, classRoom)
+			.where(
+				classRoom.id.in(teacherContainClass),
+				eqYear(request.year()),
+				eqStudentNumber(request.studentNumber()),
+				containsStudentName(request.studentName()),
+				eqSchoolName(request.schoolName())
+			);
 	}
 
 	@Override
