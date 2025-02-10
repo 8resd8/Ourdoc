5d079f38caaaf208408bbfc0d875b67a4a5a753d
김보민 <mokbee1@naver.com>
2025-02-10 16:04:24 +0900

    feat: 승인 요청 교사 목록 조회 query repo

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/teacher/repository/TeacherQueryRepository.java
@@ -1,10 +1,17 @@
 package com.ssafy.ourdoc.domain.user.teacher.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ssafy.ourdoc.domain.user.dto.TeacherQueryDto;
+import com.ssafy.ourdoc.domain.user.dto.TeacherVerificationDto;
 import com.ssafy.ourdoc.domain.user.teacher.dto.TeacherProfileResponseDto;
+import com.ssafy.ourdoc.global.common.enums.AuthStatus;
 
 public interface TeacherQueryRepository {
 	TeacherQueryDto getTeacherLoginDto(Long userId);
 
 	TeacherProfileResponseDto findTeacherProfileByUserId(Long userId);
+
+	Page<TeacherVerificationDto> findPendingTeachers(AuthStatus authStatus, Pageable pageable);
 }
