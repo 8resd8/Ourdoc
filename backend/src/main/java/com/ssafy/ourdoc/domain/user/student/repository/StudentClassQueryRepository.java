c0310dc6ec0dffe04cf75ec53dac21aef502ae62
김보민 <mokbee1@naver.com>
2025-02-10 10:40:31 +0900

    feat: 비활성 학생 정보 조회 query 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.ourdoc.domain.user.student.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.ssafy.ourdoc.domain.user.student.dto.InactiveStudentProfileResponseDto;
 import com.ssafy.ourdoc.domain.user.student.dto.StudentProfileResponseDto;
 import com.ssafy.ourdoc.domain.user.teacher.dto.StudentProfileDto;
 import com.ssafy.ourdoc.global.common.enums.Active;
@@ -16,4 +17,6 @@ public interface StudentClassQueryRepository {
 	Page<StudentProfileDto> findStudentsByClassRoomIdAndActiveAndAuthStatus(Long classId, Active active, AuthStatus authStatus, Pageable pageable);
 
 	StudentProfileResponseDto findStudentProfileByUserId(Long userId);
+
+	InactiveStudentProfileResponseDto findInactiveStudentProfileByUserId(Long userid);
 }
