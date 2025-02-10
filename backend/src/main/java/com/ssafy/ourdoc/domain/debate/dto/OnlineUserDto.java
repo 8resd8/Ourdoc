c612d5337af4d81926c849408069cf66799a0c82
방승윤 <sybang98@gmail.com>
2025-02-10 13:51:43 +0900

    feat: 독서토론방 접속자 정보 DTO

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/OnlineUserDto.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/OnlineUserDto.java
@@ -3,7 +3,7 @@ package com.ssafy.ourdoc.domain.debate.dto;
 import com.ssafy.ourdoc.global.common.enums.UserType;
 
 public record OnlineUserDto(
-	Long userId,
+	String schoolName,
 	String name,
 	UserType userType,
 	String profileImagePath
