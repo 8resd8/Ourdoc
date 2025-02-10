f3d503fb28161d7693ce37f3ada8b2379dafc6eb
김보민 <mokbee1@naver.com>
2025-02-10 17:43:52 +0900

    feat: 교사 승인 요청 수락 controller

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.ssafy.ourdoc.domain.user.controller;
 
+import com.ssafy.ourdoc.domain.user.dto.request.TeacherVerificationRequest;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.service.AdminService;
 import com.ssafy.ourdoc.domain.user.dto.TeacherVerificationDto;
@@ -8,6 +9,7 @@ import com.ssafy.ourdoc.global.common.enums.AuthStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,5 +22,11 @@ public class AdminController {
 	@GetMapping("/teachers/verification")
 	public Page<TeacherVerificationDto> getPendingTeachers(@Login User user, Pageable pageable) {
 		return adminService.getPendingTeachers(user, pageable);
+	}
+
+	@PatchMapping("/verification")
+	public ResponseEntity<String> verifyTeacher(@Login User user, @RequestBody TeacherVerificationRequest request) {
+		adminService.verifyTeacher(user, request);
+		return ResponseEntity.ok("교사 인증 요청 처리 완료");
 	}
 }
