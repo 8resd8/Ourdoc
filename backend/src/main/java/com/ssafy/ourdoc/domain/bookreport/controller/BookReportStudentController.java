13b738f8777ac0aed2528000383f0852da91aead
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-10 22:08:44 +0900

    feat: 독서록 삭제 컨트롤러 메서드 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportStudentController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/controller/BookReportStudentController.java
@@ -1,7 +1,9 @@
 package com.ssafy.ourdoc.domain.bookreport.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReadLogRequest;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportListResponse;
-import com.ssafy.ourdoc.domain.bookreport.dto.FeedbackRequest;
 import com.ssafy.ourdoc.domain.bookreport.service.BookReportStudentService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.annotation.Login;
@@ -20,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/bookreports")
+@RequestMapping("/bookreports/students")
 public class BookReportStudentController {
 
 	private final BookReportStudentService bookReportStudentService;
@@ -31,15 +32,15 @@ public class BookReportStudentController {
 		bookReportStudentService.saveBookReport(user, request);
 	}
 
-	@PostMapping("/feedback")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void createBookReportFeedback(@RequestBody FeedbackRequest request) {
-		bookReportStudentService.saveBookReportFeedback(request);
-	}
-
-	@GetMapping("/student")
+	@GetMapping
 	public BookReportListResponse getBookReportList(@Login User user, @RequestParam("grade") int grade) {
 		return bookReportStudentService.getBookReports(user, grade);
+	}
+
+	@DeleteMapping("/{bookReportId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteBookReport(@PathVariable("bookReportId") Long bookReportId) {
+		bookReportStudentService.deleteBookReport(bookReportId);
 	}
 
 }
