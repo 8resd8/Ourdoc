7454a5c69da7be760395f218035bca8a0948c155
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-10 22:03:43 +0900

    feat: 삭제 서비스코드 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/service/BookReportStudentService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/service/BookReportStudentService.java
@@ -1,11 +1,11 @@
 package com.ssafy.ourdoc.domain.bookreport.service;
 
 import static com.ssafy.ourdoc.global.common.enums.ApproveStatus.*;
-import static com.ssafy.ourdoc.global.common.enums.EvaluatorType.*;
 import static com.ssafy.ourdoc.global.common.enums.HomeworkStatus.*;
 import static com.ssafy.ourdoc.global.common.enums.NotificationType.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
@@ -14,10 +14,7 @@ import com.ssafy.ourdoc.domain.book.repository.BookRepository;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReadLogRequest;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportDto;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportListResponse;
-import com.ssafy.ourdoc.domain.bookreport.dto.FeedbackRequest;
 import com.ssafy.ourdoc.domain.bookreport.entity.BookReport;
-import com.ssafy.ourdoc.domain.bookreport.entity.BookReportFeedBack;
-import com.ssafy.ourdoc.domain.bookreport.repository.BookReportFeedbackRepository;
 import com.ssafy.ourdoc.domain.bookreport.repository.BookReportRepository;
 import com.ssafy.ourdoc.domain.notification.service.NotificationService;
 import com.ssafy.ourdoc.domain.user.entity.User;
@@ -33,12 +30,10 @@ import lombok.RequiredArgsConstructor;
 public class BookReportStudentService {
 
 	private final BookReportRepository bookReportRepository;
-	private final BookReportFeedbackRepository feedbackRepository;
 	private final StudentClassRepository studentClassRepository;
 	private final BookRepository bookRepository;
 	private final NotificationService notificationService;
 
-	// 독서록 작성
 	public void saveBookReport(User user, BookReadLogRequest request) {
 		StudentClass studentClass = studentClassRepository.findStudentClassByUserId(user.getId()).orElseThrow();
 		Book book = bookRepository.findById(request.bookId()).orElseThrow();
@@ -55,17 +50,6 @@ public class BookReportStudentService {
 		notificationService.sendNotifyStudentFromTeacher(user, 독서록); // 알림전송
 	}
 
-	// 작성한 독서록 기반 피드백 받은것 처리
-	public void saveBookReportFeedback(FeedbackRequest request) {
-		BookReport bookReport = bookReportRepository.findById(request.bookReportId()).orElseThrow();
-		// 교정 내용 반영
-		bookReport.saveAfterContent(request.afterContent());
-
-		BookReportFeedBack bookReportFeedBack = BookReportFeedBack.builder().bookReport(bookReport).type(인공지능).build();
-
-		// 피드백 내용 저장
-		feedbackRepository.save(bookReportFeedBack);
-	}
 
 	public BookReportListResponse getBookReports(User user, int grade) {
 		List<BookReport> bookReports = bookReportRepository.findByUserIdAndGrade(user.getId(), grade);
@@ -80,6 +64,13 @@ public class BookReportStudentService {
 			.toList();
 
 		return new BookReportListResponse(bookReportDtos);
+	}
+
+	public void deleteBookReport(Long bookReportId) {
+		BookReport bookReport = bookReportRepository.findById(bookReportId)
+			.orElseThrow(() -> new NoSuchElementException("지울 독서록이 없습니다."));
+
+		bookReportRepository.delete(bookReport);
 	}
 
 }
