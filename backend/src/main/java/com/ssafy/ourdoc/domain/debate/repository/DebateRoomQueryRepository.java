c05a6716ff504938164ec76f5ba8c6f5ce416f75
방승윤 <sybang98@gmail.com>
2025-02-10 13:50:12 +0900

    feat: 독서토론방 접속중인 사람 목록을 불러오는 jpa query

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepository.java
@@ -2,9 +2,13 @@ package com.ssafy.ourdoc.domain.debate.repository;
 
 import java.util.List;
 
+import com.ssafy.ourdoc.domain.debate.dto.OnlineUserDto;
 import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
 
 public interface DebateRoomQueryRepository {
 	Long countCurrentPeople(Long roomId);
+
 	List<RoomOnline> findAllActiveByRoomId(Long roomId);
+
+	List<OnlineUserDto> findOnlineUsersByRoomId(Long roomId);
 }
