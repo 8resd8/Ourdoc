05c8da1f8d0297ba181bf3dacc5d157f540b480d
방승윤 <sybang98@gmail.com>
2025-02-10 13:50:09 +0900

    feat: 독서토론방 접속중인 사람 목록을 불러오는 jpa query

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/repository/DebateRoomQueryRepositoryImpl.java
@@ -4,8 +4,18 @@ import static com.ssafy.ourdoc.domain.debate.entity.QRoomOnline.*;
 
 import java.util.List;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.ourdoc.domain.classroom.entity.QClassRoom;
+import com.ssafy.ourdoc.domain.classroom.entity.QSchool;
+import com.ssafy.ourdoc.domain.debate.dto.OnlineUserDto;
+import com.ssafy.ourdoc.domain.debate.entity.QRoomOnline;
 import com.ssafy.ourdoc.domain.debate.entity.RoomOnline;
+import com.ssafy.ourdoc.domain.user.entity.QUser;
+import com.ssafy.ourdoc.domain.user.student.entity.QStudentClass;
+import com.ssafy.ourdoc.domain.user.teacher.entity.QTeacherClass;
+import com.ssafy.ourdoc.global.common.enums.UserType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +37,47 @@ public class DebateRoomQueryRepositoryImpl implements DebateRoomQueryRepository 
 			.where(
 				roomOnline.room.id.eq(roomId)
 					.and(roomOnline.createdAt.eq(roomOnline.updatedAt))
+			).fetch();
+	}
+
+	public List<OnlineUserDto> findOnlineUsersByRoomId(Long roomId) {
+		QRoomOnline roomOnline = QRoomOnline.roomOnline;
+		QUser user = QUser.user;
+
+		QTeacherClass teacherClass = QTeacherClass.teacherClass;
+		QClassRoom teacherClassRoom = new QClassRoom("teacherClassRoom");
+		QSchool teacherSchool = new QSchool("teacherSchool");
+
+		QStudentClass studentClass = QStudentClass.studentClass;
+		QClassRoom studentClassRoom = new QClassRoom("studentClassRoom");
+		QSchool studentSchool = new QSchool("studentSchool");
+
+		return queryFactory
+			.select(
+				Projections.constructor(
+					OnlineUserDto.class,
+					Expressions.stringTemplate(
+						"CASE WHEN {0} = {1} THEN {2} ELSE {3} END",
+						roomOnline.user.userType,
+						Expressions.constant(UserType.교사),
+						teacherSchool.schoolName,
+						studentSchool.schoolName
+					),
+					roomOnline.user.name,
+					roomOnline.user.userType,
+					roomOnline.user.profileImagePath
+				)
+			).from(roomOnline)
+			.leftJoin(roomOnline.user, user)
+			.leftJoin(teacherClass).on(teacherClass.user.id.eq(roomOnline.user.id))
+			.leftJoin(teacherClass.classRoom, teacherClassRoom)
+			.leftJoin(teacherClassRoom.school, teacherSchool)
+			.leftJoin(studentClass).on(studentClass.user.id.eq(roomOnline.user.id))
+			.leftJoin(studentClass.classRoom, studentClassRoom)
+			.leftJoin(studentClassRoom.school, studentSchool)
+			.where(
+				roomOnline.room.id.eq(roomId),
+				roomOnline.createdAt.eq(roomOnline.updatedAt)
 			).fetch();
 	}
 }
