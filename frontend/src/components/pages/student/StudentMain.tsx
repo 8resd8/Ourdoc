ae59b5fce27fdc3debce0151bd1d8609ff328938
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-11 21:39:47 +0900

    feat: 독서룸 이동버튼

--- frontend/src/components/pages/student/StudentMain.tsx
+++ frontend/src/components/pages/student/StudentMain.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 const StudentMain = () => {
-  return <div>StudentMain Component</div>;
+  return <div>
+    <Link to="/debate/room">
+      debateRoom 이동<br></br>
+    </Link>
+    StudentMain Component</div>;
 };
 
 export default StudentMain;
