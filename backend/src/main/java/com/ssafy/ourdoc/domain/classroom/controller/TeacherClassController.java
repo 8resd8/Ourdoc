c7b910e53833cd4a428cfe11e7301456341b2433
RESD\dkanf <dkanfjgwls@naver.com>
2025-02-11 20:59:38 +0900

    feat: 교사 본인 근무, 학급학생 조회매핑 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/TeacherClassController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/TeacherClassController.java
@@ -2,21 +2,21 @@ package com.ssafy.ourdoc.domain.classroom.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.classroom.dto.CreateClassRequest;
 import com.ssafy.ourdoc.domain.classroom.dto.teacher.TeacherRoomResponse;
+import com.ssafy.ourdoc.domain.classroom.dto.teacher.TeacherRoomStudentResponse;
 import com.ssafy.ourdoc.domain.classroom.service.ClassService;
 import com.ssafy.ourdoc.domain.classroom.service.TeacherClassRoomService;
 import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.global.annotation.Login;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -33,11 +33,16 @@ public class TeacherClassController {
 		classService.createClass(user, request);
 	}
 
-	// 해당 교사의 전체 학교 - 학년 - 반 조회
-	@GetMapping("/year-class")
+	// 해당 교사의 연도, 학교, 학년, 반 조회
+	@GetMapping("/classes")
 	public TeacherRoomResponse getTeachersRoom(@Login User user) {
 		return teacherClassRoomService.getTeacherRoom(user);
 	}
 
-	// 해당 학년 - 반의 학생 조회
+	// 교사 반 학생 조회
+	@GetMapping("/classes/students")
+	public TeacherRoomStudentResponse getTeachersRoomStudents(@Login User user, @RequestParam(value = "year") int year) {
+		return teacherClassRoomService.getTeacherRoomStudent(user, year);
+	}
+
 }
