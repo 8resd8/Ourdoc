92fdba946fb1b0f4b104bede0372314080925878
방승윤 <sybang98@gmail.com>
2025-02-12 13:37:56 +0900

    feat: 학년/학급 많이 읽은 도서 조회 query 메서드

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/repository/BookQueryRepository.java
@@ -2,8 +2,13 @@ package com.ssafy.ourdoc.domain.book.repository;
 
 import java.util.List;
 
+import com.ssafy.ourdoc.domain.book.dto.BookMostDto;
 import com.ssafy.ourdoc.domain.book.entity.Book;
 
 public interface BookQueryRepository {
 	List<Book> findBookList(String title, String author, String publisher);
+
+	BookMostDto findBookGradeMost(Long userId);
+
+	BookMostDto findBookClassMost(Long userId);
 }
