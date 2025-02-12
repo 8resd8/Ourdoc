4810fc58be087e87d9012370b064e7b4b1c29d6d
상한규 <nic6@naver.com>
2025-02-13 07:31:36 +0900

    feat: 학생 독서록 작성 ui

--- frontend/src/components/pages/student/StudentReportWrite.tsx
+++ frontend/src/components/pages/student/StudentReportWrite.tsx
@@ -1,5 +1,71 @@
 const StudentReportWrite = () => {
-  return <div>StudentReportWrite Component</div>;
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col w-[600px]">
+        <div className="flex flex-col w-[600px]">
+          <div className="flex flex-row justify-between">
+            <img
+              src="/assets/images/writeReportTitle.png"
+              className="h-[50px] w-[120px]"
+            />
+            <img
+              src="/assets/images/writeReportLike.png"
+              className="h-[100px] w-[100px]"
+            />
+          </div>
+          <div className="text-end text-black report-font">
+            성룡 초등학교 3학년 1반 7번
+          </div>
+          <div className="flex flex-row">
+            <div className="w-[70px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              책제목
+            </div>
+            <div className="w-[530px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              은ㅇㅁㄹ
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <div className="w-[70px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              책제목
+            </div>
+            <div className="w-[130px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              김미소
+            </div>
+            <div className="w-[70px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              출판사
+            </div>
+            <div className="w-[130px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              기므므
+            </div>
+            <div className="w-[70px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              작성일
+            </div>
+            <div className="w-[130px] py-[8px] px-[8px] border border-black justify-center items-center text-center text-black report-font truncate">
+              2024.01.03
+            </div>
+          </div>
+          <div className="w-[600px] h-[543px] py-[8px] px-[8px] border border-black justify-center items-center text-black report-font break-words overflow-auto">
+            {`2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+        2024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.032024.01.03
+       `}
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <div className="inline-flex py-[12px] w-[305px] border border-secondary-500 bg-gray-50 rounded-[10px] justify-center items-center text-center text-secondary-500 text-base font-normal font-['Pretendard'] leading-normal">
+            사진 업로드
+          </div>
+          <div className="inline-flex py-[12px] w-[305px] bg-primary-500 rounded-[10px] justify-center items-center text-center text-white text-base font-normal font-['Pretendard'] leading-normal">
+            독서록 저장
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StudentReportWrite;
