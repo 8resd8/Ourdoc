f859ad012bbf5e5313681576b74b0b8fba7dfe31
상한규 <nic6@naver.com>
2025-02-12 14:12:47 +0900

    feat:학생 메인 ui

--- frontend/src/components/pages/student/StudentMain.tsx
+++ frontend/src/components/pages/student/StudentMain.tsx
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom';
+import { TrophySummarySection } from '../../molecules/TrophySummarySection';
+import { NotificationSection } from '../../molecules/NotificationSection';
+import { ClassNameHomeworkListSection } from '../../molecules/ClassHomeworkListSection';
+import { FavoriteBookListSection } from '../../molecules/FavoriteBookListSection';
+import { WriteReportButton } from '../../atoms/WriteReportButton';
+import { DebateButton } from '../../atoms/DebateButton';
 
 const StudentMain = () => {
   return (
     <div>
       <Link to="/debate/room">debateRoom 이동</Link>
-      StudentMain Component
+      <div>
+        <WriteReportButton onClick={() => {}} />
+        <DebateButton onClick={() => {}} />
+      </div>
+      <div>
+        <TrophySummarySection />
+        <NotificationSection />
+      </div>
+      <ClassNameHomeworkListSection />
+      <FavoriteBookListSection />
     </div>
   );
 };
