aa610f1b8042afd04e4466703c6c1a42ac85e49a
김보민 <mokbee1@naver.com>
2025-02-12 22:31:22 +0900

    feat: 비밀번호 변경 query

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/repository/UserQueryRepositoryImpl.java
@@ -4,6 +4,10 @@ import static com.ssafy.ourdoc.domain.user.entity.QUser.*;
 import static com.ssafy.ourdoc.domain.user.student.entity.QStudentClass.*;
 import static com.ssafy.ourdoc.domain.user.teacher.entity.QTeacherClass.*;
 
+import java.time.LocalDateTime;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.ourdoc.domain.user.entity.QUser;
 import com.ssafy.ourdoc.domain.user.entity.User;
@@ -43,9 +47,20 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
 	}
 
 	@Override
+	@Transactional
 	public void updateProfileImage(User user, String profileImageUrl) {
 		queryFactory.update(QUser.user)
 			.set(QUser.user.profileImagePath, profileImageUrl)
+			.where(QUser.user.id.eq(user.getId()))
+			.execute();
+	}
+
+	@Override
+	@Transactional
+	public void updatePassword(User user, String newHashedPassword) {
+		queryFactory.update(QUser.user)
+			.set(QUser.user.password, newHashedPassword)
+			.set(QUser.user.updatedAt, LocalDateTime.now())
 			.where(QUser.user.id.eq(user.getId()))
 			.execute();
 	}
