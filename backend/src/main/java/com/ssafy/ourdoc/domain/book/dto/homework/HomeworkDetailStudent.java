c63abdccc437de8b2f8a0f3a8e7db76cff0833b2
윤윤호 <yunho_yun@naver.com>
2025-02-13 23:35:15 +0900

    feat: 학생 숙제도서 상세 dto 재정의
    - 현재 학급 기준 정보 조회

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailStudent.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailStudent.java
@@ -1,14 +1,30 @@
 package com.ssafy.ourdoc.domain.book.dto.homework;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import com.ssafy.ourdoc.domain.book.dto.BookResponse;
+import com.ssafy.ourdoc.domain.book.entity.Homework;
 import com.ssafy.ourdoc.domain.bookreport.dto.BookReportHomeworkStudent;
 
 import lombok.Builder;
 
 @Builder
 public record HomeworkDetailStudent(
-	HomeworkDto homework,
-	List<BookReportHomeworkStudent> bookreports
+	Long homeworkId,
+	BookResponse book,
+	LocalDateTime createdAt,
+	boolean submitStatus,
+	List<BookReportHomeworkStudent> bookReports
 ) {
+	public static HomeworkDetailStudent of(Homework homework, boolean submitStatus,
+		List<BookReportHomeworkStudent> bookReports) {
+		return HomeworkDetailStudent.builder()
+			.homeworkId(homework.getId())
+			.book(BookResponse.of(homework.getBook()))
+			.createdAt(homework.getCreatedAt())
+			.submitStatus(submitStatus)
+			.bookReports(bookReports)
+			.build();
+	}
 }
