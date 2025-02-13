ed2e5bcdadfc449e065b4c64ad24b4c98324120a
young jo <a10019610@gmail.com>
2025-02-14 00:23:24 +0900

    feat: 교사 알림 페이지 API 작업1

--- frontend/src/components/pages/teacher/TeacherNoti.tsx
+++ frontend/src/components/pages/teacher/TeacherNoti.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import { NotificationDetailTile } from '../../atoms/NotificationDetailTile';
 import classes from './TeacherNoti.module.css';
 
 const TeacherNoti = () => {
+  const fetchNotifications = () => {
+    try {
+      // const response = await getNotificationsApi();
+    } catch (error) {}
+  };
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
   return (
     <div className={`${classes.root} mx-auto p-4`}>
       <div className="flex justify-between items-center mb-6">
