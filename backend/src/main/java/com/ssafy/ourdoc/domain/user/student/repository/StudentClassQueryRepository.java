8a379f6e99af3aa65084d87a7af916d8ac00460c
김보민 <mokbee1@naver.com>
2025-02-14 04:54:22 +0900

    feat: 학생 최근 학급 조회 쿼리 추가

--- backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/user/student/repository/StudentClassQueryRepository.java
@@ -1,17 +1,21 @@
 package com.ssafy.ourdoc.domain.user.student.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.ssafy.ourdoc.domain.user.entity.User;
 import com.ssafy.ourdoc.domain.user.student.dto.InactiveStudentProfileResponseDto;
 import com.ssafy.ourdoc.domain.user.student.dto.StudentProfileResponseDto;
+import com.ssafy.ourdoc.domain.user.student.entity.StudentClass;
 import com.ssafy.ourdoc.domain.user.teacher.dto.StudentPendingProfileDto;
 import com.ssafy.ourdoc.domain.user.teacher.dto.StudentProfileDto;
 import com.ssafy.ourdoc.global.common.enums.Active;
 import com.ssafy.ourdoc.global.common.enums.AuthStatus;
 
 public interface StudentClassQueryRepository {
-	long updateAuthStatusOfStudentClass(Long userId, Long classId, AuthStatus newStatus);
+	long updateAuthStatusOfStudentClass(Long userId, Long classId, Integer studentNumber, Active newActive, AuthStatus newStatus);
 
 	long updateAuthStatusOfStudent(Long id, AuthStatus authStatus);
 
@@ -22,4 +26,6 @@ public interface StudentClassQueryRepository {
 	StudentProfileResponseDto findStudentProfileByUserId(Long userId);
 
 	InactiveStudentProfileResponseDto findInactiveStudentProfileByUserId(Long userid);
+
+	Optional<StudentClass> findLatestStudentClass(User studentUser);
 }
