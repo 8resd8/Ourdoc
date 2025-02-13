0cffcc01602527c336f4ecaae17333d289f15ea7
LittleWaterDrops <littleWaterDrops@github.com>
2025-02-13 22:53:24 +0900

    fix: 알림 ui

--- frontend/src/components/pages/teacher/TeacherNoti.tsx
+++ frontend/src/components/pages/teacher/TeacherNoti.tsx
@@ -1,17 +1,21 @@
+import { AddDivider } from '../../../utils/AddDivder';
 import { NotificationDetailTile } from '../../atoms/NotificationDetailTile';
-import classes from './TeacherNoti.module.css';
 
 const TeacherNoti = () => {
   return (
-    <div className={`${classes.root} mx-auto p-4`}>
+    <div className={'flex w-[846px] flex-col mx-auto py-[56px] space-y-[40px]'}>
       <div className="flex justify-between items-center mb-6">
         <h1 className="headline-medium text-gray-800">알림함</h1>
       </div>
-      <NotificationDetailTile />
-      <NotificationDetailTile />
-      <NotificationDetailTile />
-      <NotificationDetailTile />
-      <NotificationDetailTile />
+      <AddDivider
+        itemList={[
+          <NotificationDetailTile />,
+          <NotificationDetailTile />,
+          <NotificationDetailTile />,
+          <NotificationDetailTile />,
+          <NotificationDetailTile />,
+        ]}
+      />
     </div>
   );
 };
