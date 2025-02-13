daef54a8d60855b3d8d484c0478b691bf56453af
윤윤호 <yunho_yun@naver.com>
2025-02-13 23:23:22 +0900

    feat: 숙제도서 상세 dto 빌더 메서드 정의

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailTeacher.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailTeacher.java
@@ -4,16 +4,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.ssafy.ourdoc.domain.book.dto.BookResponse;
+import com.ssafy.ourdoc.domain.book.entity.Homework;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherResponseWithId;
 
 import lombok.Builder;
 
 @Builder
 public record HomeworkDetailTeacher(
-	Long bookReportId,
+	Long homeworkId,
 	BookResponse book,
 	LocalDateTime createdAt,
 	int submitCount,
 	List<ReportTeacherResponseWithId> bookReports
 ) {
+	public static HomeworkDetailTeacher of(Homework homework, int submitCount,
+		List<ReportTeacherResponseWithId> bookReports) {
+		return HomeworkDetailTeacher.builder()
+			.homeworkId(homework.getId())
+			.book(BookResponse.of(homework.getBook()))
+			.createdAt(homework.getCreatedAt())
+			.submitCount(submitCount)
+			.bookReports(bookReports)
+			.build();
+	}
 }
