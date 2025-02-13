60a8f0ec950236cd6ae0ae86b731a7478c599d81
윤윤호 <yunho_yun@naver.com>
2025-02-13 23:15:01 +0900

    fix: 교사 숙제도서 상세 response dto 재정의
    - 현재 학급에 한하여 보여주도록 수정

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailTeacher.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/dto/homework/HomeworkDetailTeacher.java
@@ -5,17 +5,15 @@ import java.util.List;
 
 import com.ssafy.ourdoc.domain.book.dto.BookResponse;
 import com.ssafy.ourdoc.domain.bookreport.dto.teacher.ReportTeacherResponseWithId;
-import com.ssafy.ourdoc.domain.classroom.dto.SchoolClassDto;
 
 import lombok.Builder;
 
 @Builder
 public record HomeworkDetailTeacher(
-	Long id,
+	Long bookReportId,
 	BookResponse book,
 	LocalDateTime createdAt,
 	int submitCount,
-	List<SchoolClassDto> schoolClass,
-	List<ReportTeacherResponseWithId> bookreports
+	List<ReportTeacherResponseWithId> bookReports
 ) {
 }
