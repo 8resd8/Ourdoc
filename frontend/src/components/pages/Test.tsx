71def8dbb559ded9ecce0ddf6028b7344c991e1b
양영조 <a10019610@gmail.com>
2025-02-13 09:05:58 +0900

    fix: api 테스트 컴포넌트 수정

--- frontend/src/components/pages/Test.tsx
+++ frontend/src/components/pages/Test.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+import { getClassStudentListApi } from '../../services/teachersService';
+
 const Test = () => {
   console.log('Test');
+
+  const testApi = async () => {
+    try {
+      const response = await getClassStudentListApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    testApi();
+  }, []);
+
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="text-center text-9xl">
+      <h1>콘솔을 찍어서 확인하세요</h1>
     </div>
   );
 };
