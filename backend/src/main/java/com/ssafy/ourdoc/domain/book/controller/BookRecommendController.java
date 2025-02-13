d34d2db69e9d6c2b79ae8fb46531177a45d7885b
윤윤호 <yunho_yun@naver.com>
2025-02-13 13:15:35 +0900

    feat: 추천도서 정렬 기능 적용
    - 최신 추천일자 순으로 정렬

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookRecommendController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookRecommendController.java
@@ -1,6 +1,7 @@
 package com.ssafy.ourdoc.domain.book.controller;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,14 +41,16 @@ public class BookRecommendController {
 
 	@GetMapping("/teachers/recommend/grades")
 	public ResponseEntity<BookRecommendResponseTeacher> getRecommendTeacher(@ModelAttribute BookSearchRequest request,
-		@Login User user, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+		@Login User user,
+		@PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 		BookRecommendResponseTeacher response = bookRecommendService.getBookRecommendsTeacher(request, user, pageable);
 		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping("/students/recommend/grades")
 	public ResponseEntity<BookRecommendResponseStudent> getRecommendStudent(@ModelAttribute BookSearchRequest request,
-		@Login User user, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+		@Login User user,
+		@PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 		BookRecommendResponseStudent response = bookRecommendService.getBookRecommendsStudent(request, user, pageable);
 		return ResponseEntity.ok(response);
 	}
@@ -55,7 +58,8 @@ public class BookRecommendController {
 	@GetMapping("/teachers/recommend/classes")
 	public ResponseEntity<BookRecommendResponseTeacher> getRecommendTeacherClass(
 		@ModelAttribute BookSearchRequest request,
-		@Login User user, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+		@Login User user,
+		@PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 		BookRecommendResponseTeacher response = bookRecommendService.getBookRecommendsTeacherClass(request, user,
 			pageable);
 		return ResponseEntity.ok(response);
@@ -64,7 +68,8 @@ public class BookRecommendController {
 	@GetMapping("/students/recommend/classes")
 	public ResponseEntity<BookRecommendResponseStudent> getRecommendStudentClass(
 		@ModelAttribute BookSearchRequest request,
-		@Login User user, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+		@Login User user,
+		@PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 		BookRecommendResponseStudent response = bookRecommendService.getBookRecommendsStudentClass(request, user,
 			pageable);
 		return ResponseEntity.ok(response);
