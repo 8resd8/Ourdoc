c0d28444cd0998e20723e8f4eccd90cfd89152cf
김현우 <dkanfjgwls@naver.com>
2025-02-14 15:15:53 +0900

    feat: 검증 누락부분 개선

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/UpdateRoomRequest.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/UpdateRoomRequest.java
@@ -1,8 +1,15 @@
 package com.ssafy.ourdoc.domain.debate.dto;
 
+import jakarta.validation.constraints.Size;
+
 public record UpdateRoomRequest(
+	@Size(max = 250, message = "{size.max}")
 	String title,
+
+	@Size(max = 250, message = "{size.max}")
 	String password,
+
+	@Size(max = 250, message = "{size.max}")
 	Integer maxPeople
 ) {
 }
