d34c2bf0c3d6f65d197fa1b6c84fa811b81b1c4b
김현우 <dkanfjgwls@naver.com>
2025-02-14 16:40:57 +0900

    feat: 학교 저장

--- backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/SchoolController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/classroom/controller/SchoolController.java
@@ -7,12 +7,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.ourdoc.domain.classroom.dto.SchoolDto;
 import com.ssafy.ourdoc.domain.classroom.dto.SchoolResponse;
+import com.ssafy.ourdoc.domain.classroom.repository.SchoolRepository;
+import com.ssafy.ourdoc.domain.classroom.service.SchoolSaveService;
 import com.ssafy.ourdoc.domain.classroom.service.SchoolService;
+import com.ssafy.ourdoc.domain.classroom.util.CsvParser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,11 +27,22 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/schools")
 public class SchoolController {
 
+	private final SchoolSaveService schoolSaveService;
 	private final SchoolService schoolService;
+	private final CsvParser csvParser;
+	private final SchoolRepository schoolRepository;
 
 	@GetMapping
 	public ResponseEntity<Page<SchoolResponse>> findSchools(@RequestParam("schoolName") String schoolName,
 		@PageableDefault(size = 10, page = 0) Pageable pageable) {
 		return ResponseEntity.ok().body(schoolService.searchSchoolName(schoolName, pageable));
 	}
+
+	// @PostMapping
+	// public String save(@RequestParam(name = "filePath") String filePath) {
+	// 	// List<SchoolDto> schoolDtos = csvParser.parseSchoolCsv(filePath);
+	// 	schoolSaveService.saveSchools(filePath);
+	//
+	// 	return "ok";
+	// }
 }
