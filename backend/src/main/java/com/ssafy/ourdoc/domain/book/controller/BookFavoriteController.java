6c4a38a3a2159469892d6f8b87dad6d4c94243a7
윤윤호 <yunho_yun@naver.com>
2025-02-15 23:55:17 +0900

    feat: 관심도서 등록 CREATED(201) 상태 코드 설정

--- backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookFavoriteController.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/book/controller/BookFavoriteController.java
@@ -2,6 +2,7 @@ package com.ssafy.ourdoc.domain.book.controller;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.ourdoc.domain.book.dto.BookListResponse;
@@ -30,6 +32,7 @@ public class BookFavoriteController {
 	private final BookFavoriteService bookFavoriteService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public void addFavorite(@RequestBody BookRequest request, @Login User user) {
 		bookFavoriteService.addBookFavorite(request, user);
 	}
