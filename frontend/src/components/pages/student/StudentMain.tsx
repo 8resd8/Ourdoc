2576eabb4d61a94f8e855a5a0fd98ad6e3248e33
young jo <a10019610@gmail.com>
2025-02-16 23:37:49 +0900

    feat: 학생 메인 페이지 라우팅

--- frontend/src/components/pages/student/StudentMain.tsx
+++ frontend/src/components/pages/student/StudentMain.tsx
@@ -4,14 +4,20 @@ import { FavoriteBookListSection } from '../../molecules/FavoriteBookListSection
 import { WriteReportButton } from '../../atoms/WriteReportButton';
 import { DebateButton } from '../../atoms/DebateButton';
 import { StudentMainHomeworkListSection } from '../../molecules/StudentMainHomeworkListSection';
+import { useNavigate } from 'react-router-dom';
 
 const StudentMain = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex w-[1064px] flex-col mx-auto py-[56px] space-y-[40px]">
       <div
         className={`flex flex-col gap-6 bottom-[40px] right-[40px] fixed xl:top-[165px] xl:right-[calc((100vw)/12)]`}
       >
-        <WriteReportButton onClick={() => {}} />
+        <WriteReportButton
+          onClick={() => {
+            navigate('/student/book/search');
+          }}
+        />
         <DebateButton />
       </div>
       <div className="flex justify-between">
