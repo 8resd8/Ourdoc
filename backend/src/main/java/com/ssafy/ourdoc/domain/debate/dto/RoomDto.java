5bd69762164b5310dd5cfaaa5f1b33534702d920
방승윤 <sybang98@gmail.com>
2025-02-16 16:56:51 +0900

    feat: 독서토론방 조회 시 생성 시간 포함

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/RoomDto.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/RoomDto.java
@@ -1,11 +1,14 @@
 package com.ssafy.ourdoc.domain.debate.dto;
 
+import java.time.LocalDateTime;
+
 public record RoomDto(
 	Long roomId,
 	String title,
 	String creatorName,
 	int maxPeople,
 	Long currentPeople,
-	String schoolName
+	String schoolName,
+	LocalDateTime createdAt
 ) {
 }
