b3715915e7127490e9eb250c6ba5c624621b79b5
김현우 <dkanfjgwls@naver.com>
2025-02-17 13:17:58 +0900

    fix: 교사가 담당학생 상장정보를 조회할 수 없던 문제 해결

--- backend/src/main/java/com/ssafy/ourdoc/domain/award/service/AwardTeacherService.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/award/service/AwardTeacherService.java
@@ -1,15 +1,20 @@
 package com.ssafy.ourdoc.domain.award.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.ourdoc.domain.award.dto.teacher.AwardTeacherCreateRequest;
+import com.ssafy.ourdoc.domain.award.dto.teacher.AwardTeacherDto;
 import com.ssafy.ourdoc.domain.award.dto.teacher.AwardTeacherRequest;
 import com.ssafy.ourdoc.domain.award.dto.teacher.AwardTeacherResponse;
 import com.ssafy.ourdoc.domain.award.entity.Award;
 import com.ssafy.ourdoc.domain.award.repository.AwardRepository;
 import com.ssafy.ourdoc.domain.user.entity.User;
+import com.ssafy.ourdoc.domain.user.repository.UserRepository;
 import com.ssafy.ourdoc.global.integration.s3.service.S3StorageService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +26,7 @@ public class AwardTeacherService {
 
 	private final AwardRepository awardRepository;
 	private final S3StorageService s3StorageService;
+	private final UserRepository userRepository;
 
 	// 상장 생성
 	public void createAward(User user, AwardTeacherCreateRequest request, MultipartFile file) {
@@ -36,6 +42,12 @@ public class AwardTeacherService {
 	}
 
 	public AwardTeacherResponse getAwardTeachersClass(User user, AwardTeacherRequest request) {
-		return new AwardTeacherResponse(awardRepository.findTeacherClassAwards(user.getId(), request));
+		String studentLoginId = request.studentLoginId();
+		User studentUser = userRepository.findByLoginId(studentLoginId)
+			.orElseThrow(() -> new NoSuchElementException("해당 학생이 없습니다."));
+		List<AwardTeacherDto> teacherClassAwards = awardRepository.findTeacherClassAwards(user.getId(), request,
+			studentUser.getId());
+		return new AwardTeacherResponse(teacherClassAwards);
+		// return new AwardTeacherResponse(awardRepository.findTeacherClassAwards(user.getId(), request, studentUser.getId()) );
 	}
 }
