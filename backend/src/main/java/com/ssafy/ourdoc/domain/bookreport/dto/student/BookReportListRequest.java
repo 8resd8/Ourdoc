4f72419418f541fe184f893de6db4101b27418fb
김현우 <dkanfjgwls@naver.com>
2025-02-18 16:54:20 +0900

    fix: 검증 실패하는 문제 해결

--- backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/dto/student/BookReportListRequest.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/bookreport/dto/student/BookReportListRequest.java
@@ -2,16 +2,16 @@ package com.ssafy.ourdoc.domain.bookreport.dto.student;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record BookReportListRequest(
-	@NotBlank(message = "{not.blank}")
+	@NotNull(message = "{not.blank}")
 	@Min(value = 1, message = "{size.min}")
 	@Max(value = 6, message = "{size.max}")
 	int grade,
 
-	@NotBlank(message = "{not.blank}")
+	@NotNull(message = "{not.blank}")
 	@Positive(message = "{positive}")
 	Long classId
 ) {
