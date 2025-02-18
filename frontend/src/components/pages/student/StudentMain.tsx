8fef185e715055bca965eddae3610c3142661f63
상한규 <nic6@naver.com>
2025-02-18 14:05:19 +0900

    feat: 학생 스탬프 및 메인 화면 api 완료

--- frontend/src/components/pages/student/StudentMain.tsx
+++ frontend/src/components/pages/student/StudentMain.tsx
@@ -5,9 +5,34 @@ import { WriteReportButton } from '../../atoms/WriteReportButton';
 import { DebateButton } from '../../atoms/DebateButton';
 import { StudentMainHomeworkListSection } from '../../molecules/StudentMainHomeworkListSection';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {
+  getStudentRankApi,
+  StudentRankResponse,
+  studentRecentAIFeedbackApi,
+} from '../../../services/bookReportsService';
 
 const StudentMain = () => {
   const navigate = useNavigate();
+
+  const [recentFeedback, setrecentFeedback] = useState<string>('');
+  const [stampCount, setstampCount] = useState<StudentRankResponse>({
+    stampCount: 0,
+    rank: 0,
+    lastRank: 0,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const recentFeedback = await studentRecentAIFeedbackApi();
+      const studentRank = await getStudentRankApi();
+
+      setrecentFeedback(recentFeedback.content);
+      setstampCount(studentRank);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="flex w-[1064px] flex-col mx-auto py-[56px] space-y-[40px]">
       <div
@@ -21,7 +46,12 @@ const StudentMain = () => {
         <DebateButton />
       </div>
       <div className="flex justify-between">
-        <TrophySummarySection />
+        <TrophySummarySection
+          stampCount={stampCount.stampCount}
+          studentTotalCount={stampCount.lastRank}
+          myRank={stampCount.rank}
+          recentFeedback={recentFeedback}
+        />
         <NotificationSection isStudent />
       </div>
       <StudentMainHomeworkListSection />
