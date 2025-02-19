ec1ec6db836ec7548e59778b2229b17517f6aaa4
방승윤 <sybang98@gmail.com>
2025-02-19 14:06:01 +0900

    feat: 독서토론방 생성 request dto에서 maxpeople 삭제

--- backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/CreateRoomRequest.java
+++ backend/src/main/java/com/ssafy/ourdoc/domain/debate/dto/CreateRoomRequest.java
@@ -1,18 +1,12 @@
 package com.ssafy.ourdoc.domain.debate.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateRoomRequest(
-	@Size(max = 250, message = "{size.max}")
+	@Size(min = 2, max = 250, message = "{size}")
 	String title,
 
-	@Size(max = 250, message = "{size.max}")
-	String password,
-
-	@NotNull(message = "{notblank}")
-	@Max(value = 250, message = "{length.max}")
-	Integer maxPeople
+	@Size(min = 2, max = 250, message = "{size}")
+	String password
 ) {
 }
